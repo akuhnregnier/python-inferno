a1f784d1ae52064302661565a42f2cfab83b4703
Alexander Kuhn-Regnier <ahf.kuhnregnier@gmail.com>
2021-05-26 17:51:42 +0100

    Cached data loading.

--- analysis/calculate_monthly_ba.py
+++ analysis/calculate_monthly_ba.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import os
 from pathlib import Path
 
 import cartopy.crs as ccrs
@@ -6,6 +7,7 @@ import dask.array as darray
 import iris
 import matplotlib.pyplot as plt
 import numpy as np
+from joblib import Memory
 from jules_output_analysis.data import (
     cube_1d_to_2d,
     get_1d_data_cube,
@@ -21,6 +23,149 @@ from wildfires.data import Ext_MOD15A2H_fPAR, GFEDv4
 from python_inferno import inferno_io
 from python_inferno.configuration import land_pts
 from python_inferno.utils import exponential_average, temporal_nearest_neighbour_interp
+
+memory = Memory(str(Path(os.environ["EPHEMERAL"]) / "joblib_cache"), verbose=10)
+
+
+@memory.cache
+def load_data(N=None):
+    cubes = iris.load(
+        str(
+            Path(
+                # "~/tmp/new-with-antec3/JULES-ES.1p0.vn5.4.50.CRUJRA1.365.HYDE33.SPINUP0.Monthly.2000.nc"
+                # "~/tmp/JULES-ES.1p0.vn5.4.50.CRUJRA1.365.HYDE33.SPINUP0.Monthly.2009.nc"
+                "~/tmp/new-with-antec4/JULES-ES.1p0.vn5.4.50.CRUJRA1.365.HYDE33.SPINUP0.Instant.2001.nc"
+            ).expanduser()
+        )
+    )
+
+    # Extract the necessary variables.
+    t1p5m_tile = cubes.extract_cube("t1p5m")
+    q1p5m_tile = cubes.extract_cube("q1p5m")
+    pstar = cubes.extract_cube("pstar")
+    sthu_soilt = cubes.extract_cube("sthu")
+    frac = cubes.extract_cube("landCoverFrac")
+    c_soil_dpm_gb = cubes.extract_cube("c_dpm_gb")
+    c_soil_rpm_gb = cubes.extract_cube("c_rpm_gb")
+    canht = cubes.extract_cube("canht")
+    ls_rain = cubes.extract_cube("ls_rain")
+    con_rain = cubes.extract_cube("con_rain")
+    fuel_build_up = cubes.extract_cube("fuel_build")
+    fapar_diag_pft = cubes.extract_cube("fapar")
+
+    # JULES BA.
+    jules_ba_gb = cubes.extract_cube("burnt_area_gb")
+
+    jules_fapar = (
+        darray.sum(fapar_diag_pft.core_data() * frac.core_data()[:, :13], axis=1)
+        / darray.sum(frac.core_data()[:, :13], axis=1)
+    )[:, 0]
+
+    jules_lats = pstar.coord("latitude")
+    jules_lons = pstar.coord("longitude")
+
+    data_time_coord = fapar_diag_pft.coord("time")
+
+    indices_1d_to_2d = get_1d_to_2d_indices(
+        pstar.coord("latitude").points[0],
+        convert_longitudes(pstar.coord("longitude").points[0]),
+        n96e_lats,
+        n96e_lons,
+    )
+
+    # Load observed monthly BA.
+    gfed = GFEDv4()
+    gfed.limit_months(data_time_coord.cell(0).point, data_time_coord.cell(-1).point)
+    gfed.regrid(new_latitudes=n96e_lats, new_longitudes=n96e_lons, area_weighted=True)
+    gfed_ba = gfed.cube
+    gfed_ba_1d = np.ma.vstack(
+        [data[indices_1d_to_2d][np.newaxis] for data in gfed_ba.data]
+    )
+
+    # Load population density (`pop_den`).
+    # hyde_popd = Datasets(Ext_HYDE()).select_variables("popd").dataset
+    # clim_hyde_popd = hyde_popd.get_climatology_dataset(
+    #     hyde_popd.min_time, hyde_popd.max_time
+    # )
+    # pop_den = clim_hyde_popd.cube
+
+    # Load lightning data (`flash_rate`).
+    # wwlln = WWLLN()
+    # clim_wwlln = wwlln.get_climatology_dataset(wwlln.min_time, wwlln.max_time)
+    # flash_rate = clim_wwlln.cube
+
+    # Load observed monthly FAPAR.
+    fapar = Ext_MOD15A2H_fPAR()
+    fapar.limit_months(data_time_coord.cell(0).point, data_time_coord.cell(-1).point)
+    fapar.regrid(new_latitudes=n96e_lats, new_longitudes=n96e_lons, area_weighted=True)
+    mon_obs_fapar = fapar.cube
+
+    mon_obs_fapar_1d = np.ma.vstack(
+        [data[indices_1d_to_2d][np.newaxis] for data in mon_obs_fapar.data]
+    )
+    # Convert from monthly to timestep-aligned values.
+    obs_fapar_1d = temporal_nearest_neighbour_interp(
+        mon_obs_fapar_1d,
+        int(np.ceil(jules_fapar.shape[0] / mon_obs_fapar_1d.shape[0])),
+    )[: jules_fapar.shape[0]]
+
+    # Calculate the antecedent fuel build-up metric.
+    # This uses the fact that we are using data that is exported by the model every 4
+    # timesteps.
+    # Repeat the averaging procedure in order to reach convergence for a more
+    # realistic depiction of the averaged parameter.
+    obs_fuel_build_up_1d = exponential_average(
+        temporal_nearest_neighbour_interp(obs_fapar_1d, 4),
+        4.6283007e-04,
+        repetitions=10,
+    )[::4]
+
+    # Pre-load data.
+    if N is None:
+        N = frac.shape[0]
+    else:
+        assert N <= frac.shape[0]
+
+    iris.cube.CubeList(
+        [
+            t1p5m_tile[:N, :, 0],
+            q1p5m_tile[:N, :, 0],
+            pstar[:N, 0],
+            sthu_soilt[:N],
+            frac[:N, :, 0],
+            c_soil_dpm_gb[:N, 0],
+            c_soil_rpm_gb[:N, 0],
+            canht[:N, :, 0],
+            ls_rain[:N, 0],
+            con_rain[:N, 0],
+            fuel_build_up[:N, :, 0],
+            fapar_diag_pft[:N, :, 0],
+            jules_ba_gb[:N, 0],
+        ]
+    ).realise_data()
+
+    return (
+        # For most variables, return the arrays without any masks (which should just
+        # be `False` for JULES outputs).
+        t1p5m_tile[:N, :, 0].data.data,
+        q1p5m_tile[:N, :, 0].data.data,
+        pstar[:N, 0].data.data,
+        sthu_soilt[:N].data.data,
+        frac[:N, :, 0].data.data,
+        c_soil_dpm_gb[:N, 0].data.data,
+        c_soil_rpm_gb[:N, 0].data.data,
+        canht[:N, :, 0].data.data,
+        ls_rain[:N, 0].data.data,
+        con_rain[:N, 0].data.data,
+        fuel_build_up[:N, :, 0].data.data,
+        fapar_diag_pft[:N, :, 0].data.data,
+        jules_lats,
+        jules_lons,
+        gfed_ba_1d,
+        obs_fapar_1d[:N],
+        obs_fuel_build_up_1d[:N],
+        jules_ba_gb[:N, 0],
+    )
 
 
 def plot_comparison(jules_ba_gb, python_ba_gb, obs_ba, label="BA", title=""):
@@ -208,137 +353,48 @@ def run_inferno(
 
 
 def main():
-    # Load data.
-    cubes = iris.load(
-        str(
-            Path(
-                # "~/tmp/new-with-antec3/JULES-ES.1p0.vn5.4.50.CRUJRA1.365.HYDE33.SPINUP0.Monthly.2000.nc"
-                # "~/tmp/JULES-ES.1p0.vn5.4.50.CRUJRA1.365.HYDE33.SPINUP0.Monthly.2009.nc"
-                "~/tmp/new-with-antec4/JULES-ES.1p0.vn5.4.50.CRUJRA1.365.HYDE33.SPINUP0.Instant.2001.nc"
-            ).expanduser()
-        )
-    )
 
-    # Extract the necessary variables.
-    t1p5m_tile = cubes.extract_cube("t1p5m")
-    q1p5m_tile = cubes.extract_cube("q1p5m")
-    pstar = cubes.extract_cube("pstar")
-    sthu_soilt = cubes.extract_cube("sthu")
-    frac = cubes.extract_cube("landCoverFrac")
-    c_soil_dpm_gb = cubes.extract_cube("c_dpm_gb")
-    c_soil_rpm_gb = cubes.extract_cube("c_rpm_gb")
-    canht = cubes.extract_cube("canht")
-    ls_rain = cubes.extract_cube("ls_rain")
-    con_rain = cubes.extract_cube("con_rain")
-    fuel_build_up = cubes.extract_cube("fuel_build")
-    fapar_diag_pft = cubes.extract_cube("fapar")
-
-    jules_fapar = (
-        darray.sum(fapar_diag_pft.core_data() * frac.core_data()[:, :13], axis=1)
-        / darray.sum(frac.core_data()[:, :13], axis=1)
-    )[:, 0]
-
-    jules_lats = pstar.coord("latitude")
-    jules_lons = pstar.coord("longitude")
-
-    data_time_coord = fapar_diag_pft.coord("time")
-
-    indices_1d_to_2d = get_1d_to_2d_indices(
-        pstar.coord("latitude").points[0],
-        convert_longitudes(pstar.coord("longitude").points[0]),
-        n96e_lats,
-        n96e_lons,
-    )
-
-    # Load observed monthly BA.
-    gfed = GFEDv4()
-    gfed.limit_months(data_time_coord.cell(0).point, data_time_coord.cell(-1).point)
-    gfed.regrid(new_latitudes=n96e_lats, new_longitudes=n96e_lons, area_weighted=True)
-    gfed_ba = gfed.cube
-    gfed_ba_1d = np.ma.vstack(
-        [data[indices_1d_to_2d][np.newaxis] for data in gfed_ba.data]
-    )
-
-    # Load population density (`pop_den`).
-    # hyde_popd = Datasets(Ext_HYDE()).select_variables("popd").dataset
-    # clim_hyde_popd = hyde_popd.get_climatology_dataset(
-    #     hyde_popd.min_time, hyde_popd.max_time
-    # )
-    # pop_den = clim_hyde_popd.cube
-
-    # Load lightning data (`flash_rate`).
-    # wwlln = WWLLN()
-    # clim_wwlln = wwlln.get_climatology_dataset(wwlln.min_time, wwlln.max_time)
-    # flash_rate = clim_wwlln.cube
-
-    # Load observed monthly FAPAR.
-    fapar = Ext_MOD15A2H_fPAR()
-    fapar.limit_months(data_time_coord.cell(0).point, data_time_coord.cell(-1).point)
-    fapar.regrid(new_latitudes=n96e_lats, new_longitudes=n96e_lons, area_weighted=True)
-    mon_obs_fapar = fapar.cube
-
-    mon_obs_fapar_1d = np.ma.vstack(
-        [data[indices_1d_to_2d][np.newaxis] for data in mon_obs_fapar.data]
-    )
-    # Convert from monthly to timestep-aligned values.
-    obs_fapar_1d = temporal_nearest_neighbour_interp(
-        mon_obs_fapar_1d,
-        int(np.ceil(jules_fapar.shape[0] / mon_obs_fapar_1d.shape[0])),
-    )[: jules_fapar.shape[0]]
-
-    # Calculate the antecedent fuel build-up metric.
-    # This uses the fact that we are using data that is exported by the model every 4
-    # timesteps.
-    # Repeat the averaging procedure in order to reach convergence for a more
-    # realistic depiction of the averaged parameter.
-    obs_fuel_build_up_1d = exponential_average(
-        temporal_nearest_neighbour_interp(obs_fapar_1d, 4),
-        4.6283007e-04,
-        repetitions=10,
-    )[::4]
+    (
+        t1p5m_tile,
+        q1p5m_tile,
+        pstar,
+        sthu_soilt,
+        frac,
+        c_soil_dpm_gb,
+        c_soil_rpm_gb,
+        canht,
+        ls_rain,
+        con_rain,
+        fuel_build_up,
+        fapar_diag_pft,
+        jules_lats,
+        jules_lons,
+        gfed_ba_1d,
+        obs_fapar_1d,
+        obs_fuel_build_up_1d,
+        jules_ba_gb,
+    ) = load_data(N=None)
 
     # Define the ignition method (`ignition_method`).
     ignition_method = 1
 
-    # Pre-load data.
-    # N = frac.shape[0]
-    N = 50
-    assert N <= frac.shape[0]
-
-    iris.cube.CubeList(
-        [
-            t1p5m_tile[:N, :, 0],
-            q1p5m_tile[:N, :, 0],
-            pstar[:N, 0],
-            sthu_soilt[:N],
-            frac[:N, :, 0],
-            c_soil_dpm_gb[:N, 0],
-            c_soil_rpm_gb[:N, 0],
-            canht[:N, :, 0],
-            ls_rain[:N, 0],
-            con_rain[:N, 0],
-            fuel_build_up[:N, :, 0],
-            fapar_diag_pft[:N, :, 0],
-        ]
-    ).realise_data()
-
     python_ba_gb = run_inferno(
-        t1p5m_tile=t1p5m_tile[:N, :, 0].data.data,
-        q1p5m_tile=q1p5m_tile[:N, :, 0].data.data,
-        pstar=pstar[:N, 0].data.data,
-        sthu_soilt=sthu_soilt[:N].data.data,
-        frac=frac[:N, :, 0].data.data,
-        c_soil_dpm_gb=c_soil_dpm_gb[:N, 0].data.data,
-        c_soil_rpm_gb=c_soil_rpm_gb[:N, 0].data.data,
-        canht=canht[:N, :, 0].data.data,
-        ls_rain=ls_rain[:N, 0].data.data,
-        con_rain=con_rain[:N, 0].data.data,
+        t1p5m_tile=t1p5m_tile,
+        q1p5m_tile=q1p5m_tile,
+        pstar=pstar,
+        sthu_soilt=sthu_soilt,
+        frac=frac,
+        c_soil_dpm_gb=c_soil_dpm_gb,
+        c_soil_rpm_gb=c_soil_rpm_gb,
+        canht=canht,
+        ls_rain=ls_rain,
+        con_rain=con_rain,
         # Not used for ignition mode 1.
         pop_den=np.zeros((land_pts,)) - 1,
         flash_rate=np.zeros((land_pts,)) - 1,
         ignition_method=ignition_method,
-        fuel_build_up=fuel_build_up[:N, :, 0].data.data,
-        fapar_diag_pft=fapar_diag_pft[:N, :, 0].data.data,
+        fuel_build_up=fuel_build_up,
+        fapar_diag_pft=fapar_diag_pft,
         fapar_factor=-11,
         fapar_centre=0.4,
         fuel_build_up_factor=11,
@@ -347,12 +403,12 @@ def main():
         temperature_centre=300,
         jules_lats=jules_lats,
         jules_lons=jules_lons,
-        obs_fapar_1d=obs_fapar_1d[:N].data,
-        obs_fuel_build_up_1d=obs_fuel_build_up_1d[:N].data,
+        obs_fapar_1d=obs_fapar_1d.data,
+        obs_fuel_build_up_1d=obs_fuel_build_up_1d.data,
     )
 
     def average_cube(cube):
-        cube.data = np.ma.MaskedArray(cube.data, mask=obs_fuel_build_up_1d[:N].mask)
+        cube.data = np.ma.MaskedArray(cube.data, mask=obs_fuel_build_up_1d.mask)
         return cube[0].copy(data=np.mean(cube.data, axis=0))
 
     # Average the data.
@@ -360,7 +416,7 @@ def main():
 
     # Comparison plotting of mean BA.
     plot_comparison(
-        jules_ba_gb=cubes.extract_cube("burnt_area_gb")[:N, 0],
+        jules_ba_gb=jules_ba_gb,
         python_ba_gb=avg_python_ba_gb,
         obs_ba=get_1d_data_cube(
             np.mean(gfed_ba_1d, axis=0), lats=jules_lats, lons=jules_lons
