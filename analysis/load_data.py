12dc090b242e75c654f895816253ac4be1d73846
Alexander Kuhn-Regnier <ahf.kuhnregnier@gmail.com>
2021-07-14 22:47:42 +0200

    Added verification of the temporal interpolation.

--- analysis/load_data.py
+++ analysis/load_data.py
@@ -1,4 +1,12 @@
 # -*- coding: utf-8 -*-
+import iris
+import matplotlib.pyplot as plt
+import numpy as np
+from iris.coord_categorisation import add_month_number, add_year
+from jules_output_analysis.data import n96e_lats, n96e_lons
+from jules_output_analysis.utils import convert_longitudes, get_1d_to_2d_indices
+from wildfires.data import GFEDv4
+
 from python_inferno.data import load_data
 
 if __name__ == "__main__":
@@ -29,3 +37,37 @@ if __name__ == "__main__":
     ignition_method = 1
 
     timestep = 4 * 60 * 60
+
+    print(jules_time_coord.cell(-1).point)
+    print(jules_time_coord.cell(0).point)
+    cube = iris.cube.Cube(gfed_ba_1d, dim_coords_and_dims=[(jules_time_coord, 0)])
+
+    print(cube)
+
+    add_year(cube, "time")
+    add_month_number(cube, "time")
+    mon_avg = cube.aggregated_by(["year", "month_number"], iris.analysis.MEAN)
+
+    gfed = GFEDv4()
+    gfed.limit_months(jules_time_coord.cell(0).point, jules_time_coord.cell(-1).point)
+    gfed.regrid(new_latitudes=n96e_lats, new_longitudes=n96e_lons, area_weighted=True)
+    indices_1d_to_2d = get_1d_to_2d_indices(
+        np.ma.getdata(jules_lats.points[0]),
+        np.ma.getdata(convert_longitudes(jules_lons.points[0])),
+        n96e_lats,
+        n96e_lons,
+    )
+    mon_gfed_1d = np.ma.vstack(
+        [data[indices_1d_to_2d][np.newaxis] for data in gfed.cube.data]
+    )
+
+    print(mon_gfed_1d.shape)
+    assert mon_gfed_1d.shape == mon_avg.shape
+
+    for i in np.argsort(np.max(np.abs(mon_avg.data.data - mon_gfed_1d.data), axis=0))[
+        -10:
+    ]:
+        plt.figure()
+        plt.plot(mon_avg.data[:, i])
+        plt.plot(mon_gfed_1d[:, i])
+        plt.show()
