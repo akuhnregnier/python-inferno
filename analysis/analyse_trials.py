18a8a2d9f63751f68e722db8d5cbd9244d9d641f
Alexander Kuhn-Regnier <ahf.kuhnregnier@gmail.com>
2021-07-20 12:07:54 +0200

    Further analysis of trials.

--- analysis/analyse_trials.py
+++ analysis/analyse_trials.py
@@ -1,21 +1,58 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import json
+from pathlib import Path
+
 import matplotlib.pyplot as plt
 import numpy as np
 from hyperopt.mongoexp import MongoTrials
 
 if __name__ == "__main__":
-    trials = MongoTrials("mongo://localhost:1234/ba/jobs", exp_key="exp8")
+    exp_key = "exp9"
+    fig_dir = Path(f"~/tmp/trials_{exp_key}").expanduser()
+    fig_dir.mkdir(exist_ok=True, parents=False)
+
+    trials = MongoTrials("mongo://3.65.42.126:1234/ba/jobs", exp_key=exp_key)
+
     losses = np.array(trials.losses())
-    selection = np.array([l is not None and l < 1 and l > 0.8 for l in trials.losses()])
+
+    min_loss = np.min([l for l in losses if l is not None])
+
+    raw_selection = np.array([l is not None and l < 1000 for l in trials.losses()])
+    selection = np.array(
+        [l is not None and l < min_loss * 1.15 for l in trials.losses()]
+    )
 
     for name, vals in trials.vals.items():
         vals = np.array(vals)
+
         plt.figure()
         plt.title(name)
         plt.plot(
-            vals[selection], losses[selection], marker="o", alpha=0.4, linestyle=""
+            vals[selection], losses[selection], marker="o", alpha=0.2, linestyle=""
         )
         plt.xlabel(name)
         plt.ylabel("loss")
-        plt.show()
+
+        new_dir = fig_dir / "selection"
+        new_dir.mkdir(exist_ok=True)
+        plt.savefig(new_dir / f"{name}.png")
+
+        plt.figure()
+        plt.title(name)
+        plt.plot(
+            vals[raw_selection],
+            losses[raw_selection],
+            marker="o",
+            alpha=0.2,
+            linestyle="",
+        )
+        plt.xlabel(name)
+        plt.ylabel("loss")
+
+        new_dir = fig_dir / "raw"
+        new_dir.mkdir(exist_ok=True)
+        plt.savefig(new_dir / f"{name}.png")
+
+    with (fig_dir / "argmin.json").open("w") as f:
+        json.dump(trials.argmin, f, indent=4)
