183fd5785425345f5de41db1286ce1cbd549f369
Alexander Kuhn-Regnier <ahf.kuhnregnier@gmail.com>
2021-11-17 22:26:45 +0000

    Use of mgcv GAM package in R using rpy2. Partial dependence plots including error estimates. Code refactoring, including multiprocessing for faster plotting. Null model analysis applied to GAM predictions.

---
+++ R/gam.R
@@ -0,0 +1,46 @@
+library(mgcv)
+import::from(pracma, linspace)
+import::from(purrr, map)
+
+
+wrap_s = function(s) {
+  return(paste(c('s(', s, ')'), collapse=''))
+}
+
+file_append = function(s, stdout_file) {
+    cat(s, file=stdout_file, sep='\n', append=TRUE)
+}
+
+
+fit_gam = function(label, df_data, stdout_file, image_dir) {
+    set.seed(1)
+
+    if (substr(image_dir, nchar(image_dir), nchar(image_dir)) != "/") {
+        image_dir = paste0(image_dir, '/')
+    }
+
+    print("stdout file:")
+    print(stdout_file)
+
+    print("image_dir:")
+    print(image_dir)
+
+    # Fit GAM
+    indep = names(df_data)[!(names(df_data) %in% c("response"))]
+    gam_formula = as.formula(paste("response ~", paste(map(indep, wrap_s), collapse="+")))
+
+    fitted_gam = gam(gam_formula, method="REML", family=binomial(link="logit"), data=df_data)
+    file_append(paste("Index:", label), stdout_file)
+    file_append(capture.output(print(summary(fitted_gam))), stdout_file)
+
+    # Plot and save file.
+    png(
+      paste0(image_dir, paste0('gam_curves', label, '.png')),
+      width=1500, height=1000, pointsize=15
+    )
+    plot(fitted_gam, residuals=FALSE, rug=TRUE, se=FALSE, shade=FALSE, pages=1, all.terms=FALSE)
+    dev.off()
+
+    # Return fitted GAM.
+    return(fitted_gam)
+}
--- analysis/gam_analysis.py
+++ analysis/gam_analysis.py
@@ -1,24 +1,37 @@
-#!/usr/bin/env python3
+#!/mnt/small-ssd/miniconda3/envs/python-inferno/bin/python
 # -*- coding: utf-8 -*-
+
+# Use the correct R installation.
+
+# isort: off
+import os
+
+os.environ["R_HOME"] = "/mnt/small-ssd/miniconda3/envs/python-inferno/lib/R"
+# isort: on
+
 import gc
+import hashlib
 import math
 import os
 import pickle
 import sys
+from concurrent.futures import ProcessPoolExecutor, as_completed
+from datetime import datetime
 from enum import Enum
 from functools import partial, reduce
 from itertools import product
-from operator import add
 from pathlib import Path
-from pprint import pprint
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import rpy2.robjects as ro
+import scipy.stats
 from jules_output_analysis.data import cube_1d_to_2d, get_1d_data_cube
 from loguru import logger
-from pygam import GAM, LogisticGAM, s
+from rpy2.robjects import pandas2ri
+from rpy2.robjects.conversion import localconverter
 from tqdm import tqdm
 
 from python_inferno.ba_model import Status, calculate_scores, process_params
@@ -26,29 +39,11 @@ from python_inferno.cache import cache
 from python_inferno.configuration import N_pft_groups, pft_group_names, pft_groups
 from python_inferno.data import get_processed_climatological_data, load_jules_lats_lons
 from python_inferno.inferno import sigmoid
+from python_inferno.metrics import null_model_analysis
 from python_inferno.plotting import plotting
 from python_inferno.utils import memoize
 
 NoVal = Enum("NoVal", ["NoVal"])
-
-
-@cache
-def fit_gam(
-    *, X, y, lam, gam_type="logistic", distribution=None, link=None, N_features
-):
-    terms = reduce(add, (s(i) for i in range(N_features)))
-    if gam_type == "logistic":
-        gam_func = LogisticGAM
-    elif gam_type is None:
-        gam_func = partial(GAM, distribution=distribution, link=link)
-
-    gam = gam_func(
-        terms,
-        verbose=True,
-        # TODO Hyperopt parameter optimisation of `lam` parameter.
-        lam=[213] * N_features,
-    ).fit(X, y)
-    return gam
 
 
 def frac_weighted_mean(*, data, frac):
@@ -84,29 +79,11 @@ def check_params(params, key, value=NoVal.NoVal):
     return False
 
 
-if __name__ == "__main__":
-    mpl.rc_file(Path(__file__).absolute().parent / "matplotlibrc")
-    save_dir = Path("~/tmp/gam-model-analysis/").expanduser()
-    df_data_save_dir = Path("~/tmp/gam-model-df-data/").expanduser()
-    save_dir.mkdir(exist_ok=True, parents=False)
-    df_data_save_dir.mkdir(exist_ok=True, parents=False)
-    plotting = partial(plotting, save_dir=save_dir)
-
-    logger.remove()
-    logger.add(sys.stderr, level="INFO")
-
-    jules_lats, jules_lons = load_jules_lats_lons()
-
-    record_dir = Path(os.environ["EPHEMERAL"]) / "opt_record_bak"
-    assert record_dir.is_dir()
-
-    # To prevent memory accumulation during repeated calculations below.
-    memoize.active = False
-
+def read_global_param_data():
     global_params = []
     global_losses = []
 
-    for fname in tqdm(list(record_dir.glob("*")), desc="Reading opt_record files"):
+    for fname in record_dir.glob("*"):
         with fname.open("rb") as f:
             params, losses = pickle.load(f)
 
@@ -138,6 +115,199 @@ if __name__ == "__main__":
 
             global_params.append(ps)
             global_losses.append(loss)
+    return global_params, global_losses
+
+
+def file_hash_calc(path):
+    r_gam_hash = hashlib.md5()
+    r_gam_hash.update(
+        open("/home/alexander/Documents/PhD/python-inferno/R/gam.R").read().encode()
+    )
+    return {"path": path, "value": r_gam_hash.hexdigest()}
+
+
+@cache
+def gam_analysis(
+    gam_data_df,
+    # Ensure that the function is recalculated when the R source file is changed.
+    r_gam_file_data=file_hash_calc(
+        Path(__file__).resolve().parent.parent / "R" / "gam.R"
+    ),
+):
+    """GAM fitting and prediction using R.
+
+    TODO:
+        Currently there is no clear encapsulation of the R (rpy2) 'globalenv' used to
+        store objects here. This could lead to issues if rpy2 is used elsewhere in the
+        Python process prior to this function being called, as it may result in
+        unintended side effects or differences that are not picked up by the @cache
+        decorator.
+
+    """
+    ro.r["source"](str(r_gam_file_data["path"]))
+
+    # Store partial predictions (all but one variable held fixed).
+    partial_plot_vals = {}
+    partial_preds = {}
+    partial_pred_errs = {}
+
+    with localconverter(ro.default_converter + pandas2ri.converter):
+        # Call the sourced R GAM fitting function while automatically converting the
+        # pandas df to R.
+        ro.globalenv["fitted_gam"] = ro.globalenv["fit_gam"](
+            exp_key,
+            gam_data_df,
+            str(stdout_file),
+            str(save_dir),
+        )
+        gam_pred = ro.r('predict(fitted_gam, type="response")')
+        print("Gam pred:")
+        print(scipy.stats.describe(gam_pred))
+
+        N = 100
+
+        # NOTE Could be used to get from link to response scale.
+        # inv_link = ro.r("family(fitted_gam)$linkinv")
+
+        for feature_name in feature_names:
+            feature_vals = np.linspace(
+                gam_data_df[feature_name].min(), gam_data_df[feature_name].max(), N
+            )
+            ro.globalenv["grid_data_df"] = pd.DataFrame(
+                {
+                    key: feature_vals if key == feature_name else ([col.median()] * N)
+                    for key, col in gam_data_df.iteritems()
+                }
+            )
+            ro.globalenv["grid_pred"] = ro.r(
+                'predict(fitted_gam, grid_data_df, type="response", se.fit=TRUE)'
+            )
+            partial_plot_vals[feature_name] = feature_vals
+            partial_preds[feature_name] = ro.r("grid_pred$fit")
+            partial_pred_errs[feature_name] = ro.r("grid_pred$se.fit")
+
+    return gam_pred, partial_plot_vals, partial_preds, partial_pred_errs
+
+
+def partial_dependence_plots(
+    *,
+    fig_width=12.8,
+    fig_height=6.9,
+    se_factor=0.1,
+    feature_names,
+    expanded_opt_kwargs,
+    name_map,
+    partial_plot_vals,
+    partial_preds,
+    partial_pred_errs,
+    save_dir,
+    exp_key,
+):
+    # nrows * ncols >= N_features
+    # -> nrows >= N_features / ncols
+    # nrows / ncols >= fig_height / fig_width
+    # nrows / ncols = x * fig_height / fig_width
+    # nrows = x * ncols * fig_height / fig_width
+    # -> x * ncols * fig_height / fig_width >= N_features / ncols
+    # -> x * fig_height / fig_width * ncols**2 >= N_features
+    N_features = len(feature_names)
+    nrows = math.floor((1.5 * N_features * fig_height / fig_width) ** 0.5)
+    ncols = math.ceil(N_features / nrows)
+
+    fig, axes = plt.subplots(nrows, ncols, figsize=(fig_width, fig_height))
+    for (ax, feature_name) in zip(axes.ravel(), feature_names):
+        sigmoid_params = {}
+        for key, suffix in [
+            ("factors", "_factor"),
+            ("centres", "_centre"),
+            ("shapes", "_shape"),
+        ]:
+            sigmoid_params[key] = expanded_opt_kwargs[name_map[feature_name] + suffix]
+
+        assert all(len(values) == N_pft_groups for values in sigmoid_params.values())
+
+        handles = []
+
+        ax.set_title(name_map[feature_name])
+
+        gam_ax = ax.twinx()
+
+        ax.tick_params(axis="y", labelcolor="k")
+        gam_ax.tick_params(axis="y", labelcolor="tab:blue")
+
+        handles.append(
+            gam_ax.plot(
+                partial_plot_vals[feature_name],
+                partial_preds[feature_name],
+                label="GAM",
+                c="tab:blue",
+                zorder=3,
+            )[0]
+        )
+        # Standard error factor `se_factor`. Should be 2, but perhaps a lower factor
+        # is better for plotting.
+        handles.append(
+            gam_ax.fill_between(
+                partial_plot_vals[feature_name],
+                partial_preds[feature_name]
+                - se_factor * partial_pred_errs[feature_name],
+                partial_preds[feature_name]
+                + se_factor * partial_pred_errs[feature_name],
+                alpha=0.2,
+                fc="tab:blue",
+                zorder=2,
+                label=fr"$\pm{se_factor:0.1f}\ \mathrm{{SE}}$",
+            )
+        )
+
+        for (i, (factor, centre, shape)) in enumerate(zip(*sigmoid_params.values())):
+            handles.append(
+                ax.plot(
+                    partial_plot_vals[feature_name],
+                    sigmoid(partial_plot_vals[feature_name], factor, centre, shape),
+                    label=pft_group_names[i],
+                    c=f"C{i+1}",
+                )[0]
+            )
+
+        ax.legend(handles=handles)
+
+    for ax in axes.ravel()[N_features:]:
+        ax.set_axis_off()
+
+    fig.suptitle(exp_name)
+    plt.tight_layout()
+
+    plt.savefig(save_dir / f"{exp_key}_gam_curves.png")
+    plt.close()
+
+
+if __name__ == "__main__":
+    mpl.rc_file(Path(__file__).absolute().parent / "matplotlibrc")
+    save_dir = Path("~/tmp/gam-model-analysis/").expanduser()
+    save_dir.mkdir(exist_ok=True, parents=False)
+
+    stdout_dir = save_dir / "stdout"
+    stdout_dir.mkdir(exist_ok=True, parents=False)
+
+    stdout_file = (
+        stdout_dir / f"stdout_{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.txt"
+    )
+
+    plotting = partial(plotting, save_dir=save_dir)
+
+    logger.remove()
+    logger.add(sys.stderr, level="INFO")
+
+    jules_lats, jules_lons = load_jules_lats_lons()
+
+    record_dir = Path(os.environ["EPHEMERAL"]) / "opt_record_bak"
+    assert record_dir.is_dir()
+
+    # To prevent memory accumulation during repeated calculations below.
+    memoize.active = False
+
+    global_params, global_losses = read_global_param_data()
 
     df = pd.DataFrame(global_params)
     df["loss"] = global_losses
@@ -167,6 +337,15 @@ if __name__ == "__main__":
         "litter_pool": "litter_pool",
     }
 
+    dryness_descr = {1: "Dry Day", 2: "VPD & Precip"}
+    fuel_descr = {1: "Antec NPP", 2: "Leaf Litter Pool"}
+
+    dryness_keys = {1: "Dry_Day", 2: "VPD_Precip"}
+    fuel_keys = {1: "Antec_NPP", 2: "Leaf_Litter_Pool"}
+
+    executor = ProcessPoolExecutor(max_workers=10)
+    futures = []
+
     for dryness_method, fuel_build_up_method in product([1, 2], [1, 2]):
         name_map = all_name_map.copy()
         if dryness_method == 1:
@@ -185,14 +364,8 @@ if __name__ == "__main__":
         if not np.any(sel):
             continue
 
-        dryness_descr = {1: "Dry Day", 2: "VPD & Precip"}
-        fuel_descr = {1: "Antec NPP", 2: "Leaf Litter Pool"}
-
         exp_name = f"Dry:{dryness_descr[dryness_method]}, Fuel:{fuel_descr[fuel_build_up_method]}"
         logger.info(exp_name)
-
-        dryness_keys = {1: "Dry_Day", 2: "VPD_Precip"}
-        fuel_keys = {1: "Antec_NPP", 2: "Leaf_Litter_Pool"}
 
         exp_key = f"dry_{dryness_keys[dryness_method]}__fuel_{fuel_keys[fuel_build_up_method]}"
         logger.info(exp_key)
@@ -206,7 +379,7 @@ if __name__ == "__main__":
             for key, val in df_sel.iloc[min_index].to_dict().items()
             if not pd.isna(val) and key not in ("loss",)
         }
-        pprint(params)
+        # pprint(params)
 
         del params["dryness_method"]
         del params["fuel_build_up_method"]
@@ -283,104 +456,46 @@ if __name__ == "__main__":
 
         valid_mon_avg_gfed_ba_1d = np.ma.getdata(mon_avg_gfed_ba_1d)[~combined_mask]
 
+        # Create df for GAM fitting.
         X = np.zeros(((~combined_mask).sum(), len(valid_data_dict)))
         for i, valid_data in enumerate(valid_data_dict.values()):
             X[:, i] = valid_data
-        y = valid_mon_avg_gfed_ba_1d
 
-        N_features = X.shape[1]
         feature_names = list(valid_data_dict.keys())
 
-        gam = fit_gam(
-            X=X,
-            y=y,
-            lam=[213] * len(name_map),
-            gam_type=None,
-            distribution="normal",
-            link="log",
-            N_features=N_features,
+        gam_data_df = pd.DataFrame(
+            np.hstack((X, valid_mon_avg_gfed_ba_1d.reshape(-1, 1))),
+            columns=feature_names + ["response"],
         )
 
-        pd.DataFrame(
-            np.hstack((X, y.reshape(-1, 1))), columns=feature_names + ["response"]
-        ).to_csv(df_data_save_dir / (exp_key + ".csv"))
-
-        # gam = LogisticGAM(
-        #     reduce(add, (s(i) for i in range(N_features))),
-        #     verbose=True,
-        #     # TODO Hyperopt parameter optimisation of `lam` parameter.
-        #     # lam=[213] * len(name_map),
-        # # ).fit(X, y)
-        # ).gridsearch(X, y, lam=[np.logspace(1.5, 3, 3)] * N_features)
-
-        # ~30 parameters per minute
-        # X parameters for 120 minutes? -> 120*20/3 -> 800 parameters
-
-        gam.summary()
+        gam_pred, partial_plot_vals, partial_preds, partial_pred_errs = gam_analysis(
+            gam_data_df
+        )
 
         # NOTE Unsupported (for now).
         assert not single_opt_kwargs
 
-        name_roots = {"_".join(name.split("_")[:-1]) for name in expanded_opt_kwargs}
-
-        fig_width = 12.8
-        fig_height = 6.9
-
-        # nrows * ncols >= N_features
-        # -> nrows >= N_features / ncols
-        # nrows / ncols >= fig_height / fig_width
-        # nrows / ncols = x * fig_height / fig_width
-        # nrows = x * ncols * fig_height / fig_width
-        # -> x * ncols * fig_height / fig_width >= N_features / ncols
-        # -> x * fig_height / fig_width * ncols**2 >= N_features
-        nrows = math.floor((1.5 * N_features * fig_height / fig_width) ** 0.5)
-        ncols = math.ceil(N_features / nrows)
-
-        fig, axes = plt.subplots(nrows, ncols, figsize=(fig_width, fig_height))
-        for (ax, (i, term)) in zip(
-            axes.ravel(), enumerate(term for term in gam.terms if not term.isintercept)
-        ):
-            sigmoid_params = {}
-            for key, suffix in [
-                ("factors", "_factor"),
-                ("centres", "_centre"),
-                ("shapes", "_shape"),
-            ]:
-                sigmoid_params[key] = expanded_opt_kwargs[
-                    name_map[feature_names[i]] + suffix
-                ]
-
-            assert all(
-                len(values) == N_pft_groups for values in sigmoid_params.values()
+        # Partial dependence plots.
+        futures.append(
+            executor.submit(
+                partial_dependence_plots,
+                feature_names=feature_names,
+                expanded_opt_kwargs=expanded_opt_kwargs,
+                name_map=name_map,
+                partial_plot_vals=partial_plot_vals,
+                partial_preds=partial_preds,
+                partial_pred_errs=partial_pred_errs,
+                save_dir=save_dir,
+                exp_key=exp_key,
             )
+        )
 
-            ax.set_title(feature_names[i])
-
-            XX = gam.generate_X_grid(term=i)
-            xs = XX[:, i]
-            ax.plot(xs, gam.partial_dependence(term=i, X=XX), label="GAM")
-
-            for (i, (factor, centre, shape)) in enumerate(
-                zip(*sigmoid_params.values())
-            ):
-                ax.plot(
-                    xs, sigmoid(xs, factor, centre, shape), label=pft_group_names[i]
-                )
-            ax.legend()
-
-        for ax in axes.ravel()[N_features:]:
-            ax.set_axis_off()
-
-        fig.suptitle(exp_name)
-        plt.tight_layout()
-
-        plt.savefig(save_dir / f"{exp_key}_gam_curves.png")
-        plt.close()
+        # Further data processing for map plots etc...
 
         pred_y_1d = np.ma.MaskedArray(
             np.zeros_like(combined_mask, dtype=np.float64), mask=True
         )
-        pred_y_1d[~combined_mask] = gam.predict(X)
+        pred_y_1d[~combined_mask] = gam_pred
         mon_avg_gfed_ba_1d.mask |= combined_mask
 
         # TODO NOTE How to tie this in GAM analysis??
@@ -416,5 +531,33 @@ if __name__ == "__main__":
         )
         gc.collect()
 
-    for exp_name, data in tqdm(list(plot_data.items()), desc="Plotting"):
-        plotting(exp_name=exp_name, **data)
+    reference_obs = cube_1d_to_2d(
+        get_1d_data_cube(mon_avg_gfed_ba_1d, lats=jules_lats, lons=jules_lons)
+    ).data
+    for exp_name, data in plot_data.items():
+        futures.append(
+            executor.submit(
+                plotting,
+                exp_name=exp_name,
+                ref_2d_data=(reference_obs if exp_name != "GFED4" else None),
+                **data,
+            )
+        )
+
+    null_model_analysis(
+        reference_data=reference_obs,
+        comp_data={
+            key: vals["model_ba_2d_data"]
+            for key, vals in plot_data.items()
+            if key != "GFED4"
+        },
+        rng=np.random.default_rng(0),
+        save_dir=save_dir,
+    )
+
+    for _ in tqdm(
+        as_completed(futures), total=len(futures), desc="Waiting for executor"
+    ):
+        pass
+
+    executor.shutdown()
