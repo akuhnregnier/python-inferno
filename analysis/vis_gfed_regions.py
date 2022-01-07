a06a1b1d588ba410936188aaaac264581872f920
Alexander Kuhn-Regnier <ahf.kuhnregnier@gmail.com>
2022-01-07 18:51:30 +0100

    Fixed vis_gfed_regions.

--- analysis/vis_gfed_regions.py
+++ analysis/vis_gfed_regions.py
@@ -7,6 +7,6 @@ from wildfires.analysis import cube_plotting
 from python_inferno.plotting import get_gfed_regions
 
 if __name__ == "__main__":
-    gfed_regions = get_gfed_regions()
-    cube_plotting(gfed_regions, boundaries=np.arange(0, 15) + 0.5)
+    gfed_regions, N_regions = get_gfed_regions()
+    cube_plotting(gfed_regions, boundaries=np.arange(0, N_regions + 1) + 0.5)
     plt.show()
