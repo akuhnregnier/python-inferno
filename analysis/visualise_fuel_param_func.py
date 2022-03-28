16e0f7e75d4533d26ba8c329f4791c026014c128
Alexander Kuhn-Regnier <ahf.kuhnregnier@gmail.com>
2022-03-28 17:59:00 +0100

    Generalised sigmoid vis.

--- analysis/visualise_fuel_param_func.py
+++ analysis/visualise_fuel_param_func.py
@@ -1,15 +1,43 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import math
+from pathlib import Path
+
 import matplotlib.pyplot as plt
 import numpy as np
 
 from python_inferno.inferno import sigmoid
 
 if __name__ == "__main__":
-    factor = -1.1
-    centre = -0.98
-    shape = 1.2
-    x = np.linspace(-1, 1, 100)
-    plt.figure()
-    plt.plot(x, sigmoid(x, factor, centre, shape))
-    plt.show()
+    factors = [-1, 0.5, 3]
+    centres = [0]
+    shapes = [0.2, 1, 4]
+
+    nplots = len(factors) * len(centres) * len(shapes)
+    nrows = math.floor(nplots**0.5)
+    ncols = math.ceil(nplots / nrows)
+
+    fig, axes = plt.subplots(
+        nrows,
+        ncols,
+        figsize=np.array([1, 1]) + np.array([2.5, 1.5]) * np.array([ncols, nrows]),
+        sharex=True,
+        sharey=True,
+    )
+
+    x = np.linspace(-10, 10, 1000)
+
+    i = 0
+    for factor in factors:
+        for centre in centres:
+            for shape in shapes:
+                ax = axes.ravel()[i]
+                ax.plot(x, sigmoid(x, factor, centre, shape))
+                ax.set_title(
+                    rf"$\alpha={factor:0.1f}\ \beta={centre:0.1f}\ \gamma={shape:0.1f}$"
+                )
+                i += 1
+
+    plt.tight_layout()
+    fig.savefig(Path("~/tmp/sigmoid_curves.pdf").expanduser())
+    plt.close(fig)
