Added weights parameters. Fixed dictionary assignment in calculate_scores. Moved frac-weighted mean calculation of GPU BA. Adjusted test tolerances to the inclusion of the weights in the shader code (even though they should be no-ops with the given weights=1, additional floating point operations seem to increase the error somewhat).

src/python_inferno/ba_model.py
@@ -131,26 +131,26 @@ def calculate_scores(
 
     if Metrics.MPD in requested:
         mpd_val, ignored = calculate_mpd(avg_ba, mon_avg_gfed_ba_1d)
-        scores[mpd] = mpd_val
-        scores[mpd_ignored] = ignored
+        scores["mpd"] = mpd_val
+        scores["mpd_ignored"] = ignored
 
     if Metrics.R2 in requested:
-        scores[r2] = r2_score(y_true=y_true, y_pred=y_pred)
+        scores["r2"] = r2_score(y_true=y_true, y_pred=y_pred)
 
     if Metrics.NME in requested:
-        scores[nme] = nme(obs=y_true, pred=y_pred)
+        scores["nme"] = nme(obs=y_true, pred=y_pred)
 
     if Metrics.ARCSINH_NME in requested:
-        scores[arcsinh_nme] = nme(
+        scores["arcsinh_nme"] = nme(
             obs=np.arcsinh(ARCSINH_FACTOR * y_true),
             pred=np.arcsinh(ARCSINH_FACTOR * y_pred),
         )
 
     if Metrics.NMSE in requested:
-        scores[nmse] = nmse(obs=y_true, pred=y_pred)
+        scores["nmse"] = nmse(obs=y_true, pred=y_pred)
 
     if Metrics.LOGHIST in requested:
-        scores[loghist] = loghist(
+        scores["loghist"] = loghist(
             obs=y_true, pred=y_pred, edges=np.linspace(0, 0.4, 20)
         )
 
@@ -270,7 +270,7 @@ class BAModel:
     def process_kwargs(self, **kwargs):
         n_params = N_pft_groups
         dtype_params = np.float64
-        dummy_params = np.zeros(n_params, dtype=dtype_params)
+        dummy_params = np.ones(n_params, dtype=dtype_params)
 
         processed_kwargs = dict(overall_scale=kwargs.pop("overall_scale", 1.0))
 
@@ -286,9 +286,25 @@ class BAModel:
         # The below may conditionally be present. If not, they need to be provided
         # by 'dummy' variables.
         for keys, condition in (
-            (["fapar_factor", "fapar_centre", "fapar_shape"], True),
             (
-                ["temperature_factor", "temperature_centre", "temperature_shape"],
+                # Always process these keys.
+                [
+                    "fapar_factor",
+                    "fapar_centre",
+                    "fapar_shape",
+                    "fapar_weight",
+                    "dryness_weight",
+                    "fuel_weight",
+                ],
+                True,
+            ),
+            (
+                [
+                    "temperature_factor",
+                    "temperature_centre",
+                    "temperature_shape",
+                    "temperature_weight",
+                ],
                 self.include_temperature == 1,
             ),
             (
@@ -384,9 +400,7 @@ class BAModel:
 
 class GPUBAModel(BAModel):
     def __init__(self, **kwargs):
-        from .py_gpu_inferno import GPUInferno, frac_weighted_mean
-
-        self.frac_weighted_mean = frac_weighted_mean
+        from .py_gpu_inferno import GPUInferno
 
         logger.info("GPUBAModel init.")
 
@@ -442,10 +456,14 @@ class GPUBAModel(BAModel):
         litter_pool_factor,
         litter_pool_centre,
         litter_pool_shape,
+        fapar_weight,
+        dryness_weight,
+        temperature_weight,
+        fuel_weight,
         **kwargs,
     ):
         # TODO Eliminate need for dtype transform.
-        out = transform_dtype(self._gpu_inferno.run)(
+        return transform_dtype(self._gpu_inferno.run)(
             overall_scale=overall_scale,
             fapar_factor=fapar_factor,
             fapar_centre=fapar_centre,
@@ -465,12 +483,11 @@ class GPUBAModel(BAModel):
             litter_pool_factor=litter_pool_factor,
             litter_pool_centre=litter_pool_centre,
             litter_pool_shape=litter_pool_shape,
+            fapar_weight=fapar_weight,
+            dryness_weight=dryness_weight,
+            temperature_weight=temperature_weight,
+            fuel_weight=fuel_weight,
         )
-
-        weighted = self.frac_weighted_mean(
-            data=out.reshape((self.Nt, npft, land_pts)), frac=self.data_dict["frac"]
-        )
-        return weighted
 
 
 def gen_to_optimise(

src/python_inferno/py_gpu_inferno/__init__.py
@@ -36,6 +36,8 @@ class GPUInferno:
         dry_days,
     ):
         self.gpu_compute = _GPUCompute()
+        self.Nt = Nt
+        self.frac = frac
 
         self.gpu_compute.set_data(
             _ignitionMethod=ignition_method,
@@ -85,6 +87,10 @@ class GPUInferno:
         litter_pool_factor,
         litter_pool_centre,
         litter_pool_shape,
+        fapar_weight,
+        dryness_weight,
+        temperature_weight,
+        fuel_weight,
     ):
         self.gpu_compute.set_params(
             fapar_factor=fapar_factor,
@@ -105,9 +111,16 @@ class GPUInferno:
             litter_pool_factor=litter_pool_factor,
             litter_pool_centre=litter_pool_centre,
             litter_pool_shape=litter_pool_shape,
+            fapar_weight=fapar_weight,
+            dryness_weight=dryness_weight,
+            temperature_weight=temperature_weight,
+            fuel_weight=fuel_weight,
         )
 
-        return overall_scale * self.gpu_compute.run()
+        return overall_scale * frac_weighted_mean(
+            data=self.gpu_compute.run().reshape((self.Nt, npft, land_pts)),
+            frac=self.frac.reshape((self.Nt, n_total_pft, land_pts)),
+        )
 
     def release(self):
         self.gpu_compute.release()
@@ -124,103 +137,3 @@ def frac_weighted_mean(*, data, frac):
     #     frac[:, : data.shape[1]], axis=1
     # )
     return np.sum(data * frac[:, : data.shape[1]], axis=1)
-
-
-def run_single_shot(
-    *,
-    # Init params.
-    ignition_method,
-    flammability_method,
-    dryness_method,
-    fuel_build_up_method,
-    include_temperature,
-    t1p5m_tile,
-    q1p5m_tile,
-    pstar,
-    sthu_soilt,
-    frac,
-    c_soil_dpm_gb,
-    c_soil_rpm_gb,
-    canht,
-    ls_rain,
-    con_rain,
-    pop_den,
-    flash_rate,
-    fuel_build_up,
-    fapar_diag_pft,
-    grouped_dry_bal,
-    litter_pool,
-    dry_days,
-    # Per-run params.
-    fapar_factor,
-    fapar_centre,
-    fapar_shape,
-    fuel_build_up_factor,
-    fuel_build_up_centre,
-    fuel_build_up_shape,
-    temperature_factor,
-    temperature_centre,
-    temperature_shape,
-    dry_day_factor,
-    dry_day_centre,
-    dry_day_shape,
-    dry_bal_factor,
-    dry_bal_centre,
-    dry_bal_shape,
-    litter_pool_factor,
-    litter_pool_centre,
-    litter_pool_shape,
-):
-    Nt = pstar.shape[0]
-
-    compute = GPUInferno(
-        ignition_method=ignition_method,
-        flammability_method=flammability_method,
-        dryness_method=dryness_method,
-        fuel_build_up_method=fuel_build_up_method,
-        include_temperature=include_temperature,
-        Nt=Nt,
-        t1p5m_tile=t1p5m_tile.ravel(),
-        q1p5m_tile=q1p5m_tile.ravel(),
-        pstar=pstar.ravel(),
-        sthu_soilt_single=sthu_soilt[:, 0, 0].ravel(),
-        frac=frac.ravel(),
-        c_soil_dpm_gb=c_soil_dpm_gb.ravel(),
-        c_soil_rpm_gb=c_soil_rpm_gb.ravel(),
-        canht=canht.ravel(),
-        ls_rain=ls_rain.ravel(),
-        con_rain=con_rain.ravel(),
-        pop_den=pop_den.ravel(),
-        flash_rate=flash_rate.ravel(),
-        fuel_build_up=fuel_build_up.ravel(),
-        fapar_diag_pft=fapar_diag_pft.ravel(),
-        grouped_dry_bal=grouped_dry_bal.ravel(),
-        litter_pool=litter_pool.ravel(),
-        dry_days=dry_days.ravel(),
-    )
-
-    out = compute.run(
-        fapar_factor=fapar_factor,
-        fapar_centre=fapar_centre,
-        fapar_shape=fapar_shape,
-        fuel_build_up_factor=fuel_build_up_factor,
-        fuel_build_up_centre=fuel_build_up_centre,
-        fuel_build_up_shape=fuel_build_up_shape,
-        temperature_factor=temperature_factor,
-        temperature_centre=temperature_centre,
-        temperature_shape=temperature_shape,
-        dry_day_factor=dry_day_factor,
-        dry_day_centre=dry_day_centre,
-        dry_day_shape=dry_day_shape,
-        dry_bal_factor=dry_bal_factor,
-        dry_bal_centre=dry_bal_centre,
-        dry_bal_shape=dry_bal_shape,
-        litter_pool_factor=litter_pool_factor,
-        litter_pool_centre=litter_pool_centre,
-        litter_pool_shape=litter_pool_shape,
-    )
-
-    compute.release()
-
-    weighted = frac_weighted_mean(data=out.reshape((Nt, npft, land_pts)), frac=frac)
-    return weighted

tests/test_ba_model.py
@@ -8,7 +8,17 @@ from python_inferno.ba_model import BAModel
 def test_BAModel(params_model_ba):
     for params, expected_model_ba in params_model_ba:
         assert np.allclose(
-            BAModel(**params).run(**params)["model_ba"],
+            BAModel(**params).run(
+                **{
+                    **dict(
+                        fapar_weight=1,
+                        dryness_weight=1,
+                        temperature_weight=1,
+                        fuel_weight=1,
+                    ),
+                    **params,
+                }
+            )["model_ba"],
             expected_model_ba["python"],
             atol=1e-12,
             rtol=1e-7,

tests/test_compare_metal_python_ba_model.py
@@ -13,7 +13,7 @@ from python_inferno.ba_model import BAModel, GPUBAModel
             "dry_Dry_Day__fuel_Antec_NPP",
             {
                 "np.abs(np.mean(diffs))": 3.5e-16,
-                "np.mean(np.abs(diffs))": 4.0e-16,
+                "np.mean(np.abs(diffs))": 4.8e-16,
                 "np.max(np.abs(diffs))": 3.1e-13,
             },
         ),
@@ -21,23 +21,23 @@ from python_inferno.ba_model import BAModel, GPUBAModel
             "dry_Dry_Day__fuel_Leaf_Litter_Pool",
             {
                 "np.abs(np.mean(diffs))": 2.4e-15,
-                "np.mean(np.abs(diffs))": 2.6e-15,
+                "np.mean(np.abs(diffs))": 3.1e-15,
                 "np.max(np.abs(diffs))": 3.9e-13,
             },
         ),
         (
             "dry_VPD_Precip__fuel_Antec_NPP",
             {
-                "np.abs(np.mean(diffs))": 4.5e-15,
-                "np.mean(np.abs(diffs))": 7.8e-15,
+                "np.abs(np.mean(diffs))": 4.6e-15,
+                "np.mean(np.abs(diffs))": 9.1e-15,
                 "np.max(np.abs(diffs))": 4.8e-13,
             },
         ),
         (
             "dry_VPD_Precip__fuel_Leaf_Litter_Pool",
             {
-                "np.abs(np.mean(diffs))": 2.4e-17,
-                "np.mean(np.abs(diffs))": 4.8e-17,
+                "np.abs(np.mean(diffs))": 1.5e-16,
+                "np.mean(np.abs(diffs))": 8.1e-16,
                 "np.max(np.abs(diffs))": 1.5e-13,
             },
         ),
@@ -56,7 +56,17 @@ def test_GPUBAModel(model_params, exp_key, expected_diffs):
         # kwargs they require, ignoring the rest.
         ba_model = model_class(**params)
 
-        model_bas[name] = ba_model.run(**params)["model_ba"]
+        model_bas[name] = ba_model.run(
+            **{
+                **dict(
+                    fapar_weight=1,
+                    dryness_weight=1,
+                    temperature_weight=1,
+                    fuel_weight=1,
+                ),
+                **params,
+            }
+        )["model_ba"]
 
         if name == "metal":
             ba_model._gpu_inferno.release()

tests/test_py_gpu_inferno.py
@@ -34,7 +34,6 @@ def test_GPUCompute_synthetic(
     flammabilityMethod,
     Nt,
 ):
-
     compute.set_data(
         _ignitionMethod=1,
         _flammabilityMethod=flammabilityMethod,
@@ -80,6 +79,10 @@ def test_GPUCompute_synthetic(
         litter_pool_factor=np.zeros(N_pft_groups, dtype=np.float32),
         litter_pool_centre=np.zeros(N_pft_groups, dtype=np.float32),
         litter_pool_shape=np.zeros(N_pft_groups, dtype=np.float32),
+        fapar_weight=np.ones(N_pft_groups, dtype=np.float32),
+        dryness_weight=np.ones(N_pft_groups, dtype=np.float32),
+        temperature_weight=np.ones(N_pft_groups, dtype=np.float32),
+        fuel_weight=np.ones(N_pft_groups, dtype=np.float32),
     )
 
     compute.run()
@@ -87,9 +90,15 @@ def test_GPUCompute_synthetic(
 
 def test_GPUBAModel(params_model_ba):
     for params, expected_model_ba in params_model_ba:
-        assert np.allclose(
-            GPUBAModel(**params).run(**params)["model_ba"],
-            expected_model_ba["metal"],
-            atol=1e-12,
-            rtol=1e-7,
-        )
+        model_ba = GPUBAModel(**params).run(
+            **{
+                **dict(
+                    fapar_weight=1,
+                    dryness_weight=1,
+                    temperature_weight=1,
+                    fuel_weight=1,
+                ),
+                **params,
+            }
+        )["model_ba"]
+        assert np.allclose(model_ba, expected_model_ba["metal"], atol=1e-12, rtol=1e-7)