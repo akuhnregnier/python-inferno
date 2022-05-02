9d7b742e406d43d67a5125f2b50754b1a2c38810
Alexander Kuhn-Regnier <ahf.kuhnregnier@gmail.com>
2022-05-02 20:27:58 +0100

    Added test for many repeated runs. Refactored test data creation.

--- tests/test_py_gpu_inferno.py
+++ tests/test_py_gpu_inferno.py
@@ -21,46 +21,47 @@ def compute():
     compute.release()
 
 
-@pytest.mark.parametrize("includeTemperature", [0, 1])
-@pytest.mark.parametrize("fuelBuildUpMethod", [1, 2])
-@pytest.mark.parametrize("drynessMethod", [1, 2])
-@pytest.mark.parametrize("flammabilityMethod", [1, 2])
-@pytest.mark.parametrize("Nt", [100, 10])
-def test_GPUCompute_synthetic(
-    compute,
-    includeTemperature,
-    fuelBuildUpMethod,
-    drynessMethod,
-    flammabilityMethod,
-    Nt,
-):
-    compute.set_data(
-        _ignitionMethod=1,
-        _flammabilityMethod=flammabilityMethod,
-        _drynessMethod=drynessMethod,
-        _fuelBuildUpMethod=fuelBuildUpMethod,
-        _includeTemperature=includeTemperature,
-        _Nt=Nt,
-        t1p5m_tile=np.zeros(n_cell_tot_pft(Nt), dtype=np.float32) + 300,
-        q1p5m_tile=np.zeros(n_cell_tot_pft(Nt), dtype=np.float32),
-        pstar=np.zeros(n_cell_no_pft(Nt), dtype=np.float32),
-        sthu_soilt_single=np.zeros(n_cell_no_pft(Nt), dtype=np.float32),
-        frac=np.zeros(n_cell_tot_pft(Nt), dtype=np.float32),
-        c_soil_dpm_gb=np.zeros(n_cell_no_pft(Nt), dtype=np.float32),
-        c_soil_rpm_gb=np.zeros(n_cell_no_pft(Nt), dtype=np.float32),
-        canht=np.zeros(n_cell_nat_pft(Nt), dtype=np.float32),
-        ls_rain=np.zeros(n_cell_no_pft(Nt), dtype=np.float32),
-        con_rain=np.zeros(n_cell_no_pft(Nt), dtype=np.float32),
-        pop_den=np.zeros(n_cell_no_pft(Nt), dtype=np.float32),
-        flash_rate=np.zeros(n_cell_no_pft(Nt), dtype=np.float32),
-        fuel_build_up=np.zeros(n_cell_nat_pft(Nt), dtype=np.float32),
-        fapar_diag_pft=np.zeros(n_cell_nat_pft(Nt), dtype=np.float32),
-        grouped_dry_bal=np.zeros(n_cell_grp_pft(Nt), dtype=np.float32),
-        litter_pool=np.zeros(n_cell_nat_pft(Nt), dtype=np.float32),
-        dry_days=np.zeros(n_cell_no_pft(Nt), dtype=np.float32),
-    )
+@pytest.fixture
+def get_compute_data():
+    def _get_compute_data(
+        includeTemperature=1,
+        fuelBuildUpMethod=1,
+        drynessMethod=1,
+        flammabilityMethod=1,
+        Nt=10,
+    ):
+        return dict(
+            _ignitionMethod=1,
+            _flammabilityMethod=flammabilityMethod,
+            _drynessMethod=drynessMethod,
+            _fuelBuildUpMethod=fuelBuildUpMethod,
+            _includeTemperature=includeTemperature,
+            _Nt=Nt,
+            t1p5m_tile=np.zeros(n_cell_tot_pft(Nt), dtype=np.float32) + 300,
+            q1p5m_tile=np.zeros(n_cell_tot_pft(Nt), dtype=np.float32),
+            pstar=np.zeros(n_cell_no_pft(Nt), dtype=np.float32),
+            sthu_soilt_single=np.zeros(n_cell_no_pft(Nt), dtype=np.float32),
+            frac=np.zeros(n_cell_tot_pft(Nt), dtype=np.float32),
+            c_soil_dpm_gb=np.zeros(n_cell_no_pft(Nt), dtype=np.float32),
+            c_soil_rpm_gb=np.zeros(n_cell_no_pft(Nt), dtype=np.float32),
+            canht=np.zeros(n_cell_nat_pft(Nt), dtype=np.float32),
+            ls_rain=np.zeros(n_cell_no_pft(Nt), dtype=np.float32),
+            con_rain=np.zeros(n_cell_no_pft(Nt), dtype=np.float32),
+            pop_den=np.zeros(n_cell_no_pft(Nt), dtype=np.float32),
+            flash_rate=np.zeros(n_cell_no_pft(Nt), dtype=np.float32),
+            fuel_build_up=np.zeros(n_cell_nat_pft(Nt), dtype=np.float32),
+            fapar_diag_pft=np.zeros(n_cell_nat_pft(Nt), dtype=np.float32),
+            grouped_dry_bal=np.zeros(n_cell_grp_pft(Nt), dtype=np.float32),
+            litter_pool=np.zeros(n_cell_nat_pft(Nt), dtype=np.float32),
+            dry_days=np.zeros(n_cell_no_pft(Nt), dtype=np.float32),
+        )
 
-    compute.set_params(
+    return _get_compute_data
+
+
+@pytest.fixture
+def compute_params():
+    return dict(
         fapar_factor=np.zeros(N_pft_groups, dtype=np.float32),
         fapar_centre=np.zeros(N_pft_groups, dtype=np.float32),
         fapar_shape=np.zeros(N_pft_groups, dtype=np.float32),
@@ -85,7 +86,43 @@ def test_GPUCompute_synthetic(
         fuel_weight=np.ones(N_pft_groups, dtype=np.float32),
     )
 
+
+@pytest.mark.parametrize("includeTemperature", [0, 1])
+@pytest.mark.parametrize("fuelBuildUpMethod", [1, 2])
+@pytest.mark.parametrize("drynessMethod", [1, 2])
+@pytest.mark.parametrize("flammabilityMethod", [1, 2])
+@pytest.mark.parametrize("Nt", [100, 10])
+def test_GPUCompute_synthetic(
+    get_compute_data,
+    compute_params,
+    compute,
+    includeTemperature,
+    fuelBuildUpMethod,
+    drynessMethod,
+    flammabilityMethod,
+    Nt,
+):
+    compute.set_data(
+        **get_compute_data(
+            includeTemperature=includeTemperature,
+            fuelBuildUpMethod=fuelBuildUpMethod,
+            drynessMethod=drynessMethod,
+            flammabilityMethod=flammabilityMethod,
+            Nt=Nt,
+        )
+    )
+
+    compute.set_params(**compute_params)
+
     compute.run()
+
+
+@pytest.mark.timeout(45)
+def test_many_run(compute, get_compute_data, compute_params):
+    compute.set_data(**get_compute_data())
+    for i in range(10000):
+        compute.set_params(**compute_params)
+        compute.run()
 
 
 def test_GPUBAModel(params_model_ba):
