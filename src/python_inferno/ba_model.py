GPU Inferno with integrated conservative temporal averaging. Fixed bug where weights parameters were passed to the flammability calculation in the wrong order! Calculation of diagnostics and check mask. Refactored properties for conservative averaging since they are stored in the internal GPU compute class and not automatically updated.

src/python_inferno/ba_model.py
@@ -9,7 +9,7 @@ from sklearn.metrics import r2_score
 from .configuration import N_pft_groups, land_pts, npft
 from .data import get_processed_climatological_data, timestep
 from .metrics import Metrics, loghist, nmse
-from .multi_timestep_inferno import multi_timestep_inferno
+from .multi_timestep_inferno import _get_diagnostics, multi_timestep_inferno
 from .py_gpu_inferno import GPUCalculateMPD, cpp_nme
 from .utils import (
     ConsMonthlyAvgNoMask,
@@ -66,7 +66,7 @@ def run_model(
     land_point=-1,
     **kwargs,
 ):
-    model_ba = unpack_wrapped(multi_timestep_inferno)(
+    return unpack_wrapped(multi_timestep_inferno)(
         ignition_method=1,
         timestep=timestep,
         flammability_method=2,
@@ -82,7 +82,6 @@ def run_model(
         **data_dict,
         **kwargs,
     )
-    return model_ba
 
 
 def calculate_mpd(avg_ba, mon_avg_gfed_ba_1d, mpd=GPUCalculateMPD(land_pts).run):
@@ -366,6 +365,33 @@ class BAModel:
             **processed_kwargs,
         )
 
+    def _get_checks_failed_mask(
+        self,
+        *,
+        crop_f,
+        **kwargs,
+    ):
+        processed_kwargs = self.process_kwargs(**kwargs)
+        _, checks_failed_mask = BAModel.get_model_ba(
+            self,
+            dryness_method=self.dryness_method,
+            fuel_build_up_method=self.fuel_build_up_method,
+            include_temperature=self.include_temperature,
+            data_dict=self.data_dict,
+            return_checks_failed_mask=True,
+            **processed_kwargs,
+        )
+        return checks_failed_mask
+
+    def _get_diagnostics(self):
+        return _get_diagnostics(
+            t1p5m_tile=self.data_dict["t1p5m_tile"],
+            q1p5m_tile=self.data_dict["q1p5m_tile"],
+            pstar=self.data_dict["pstar"],
+            ls_rain=self.data_dict["ls_rain"],
+            con_rain=self.data_dict["con_rain"],
+        )
+
     def run(
         self,
         *,
@@ -419,7 +445,6 @@ class BAModel:
 
 class GPUBAModel(BAModel):
     def __init__(self, **kwargs):
-        from .py_gpu_inferno import GPUInferno
 
         logger.info("GPUBAModel init.")
 
@@ -427,7 +452,7 @@ class GPUBAModel(BAModel):
 
         self.Nt = self.data_dict["pstar"].shape[0]
 
-        self._gpu_inferno = transform_dtype(GPUInferno)(
+        self._gpu_inferno = transform_dtype(self._gpu_class)(
             ignition_method=1,
             flammability_method=2,
             dryness_method=self.dryness_method,
@@ -451,7 +476,14 @@ class GPUBAModel(BAModel):
             grouped_dry_bal=self.data_dict["grouped_dry_bal"].ravel(),
             litter_pool=self.data_dict["litter_pool"].ravel(),
             dry_days=self.data_dict["dry_days"].ravel(),
+            checks_failed=BAModel._get_checks_failed_mask(self, **kwargs),
         )
+
+    @property
+    def _gpu_class(self):
+        from .py_gpu_inferno import GPUInferno
+
+        return GPUInferno
 
     def get_model_ba(
         self,
@@ -508,8 +540,34 @@ class GPUBAModel(BAModel):
             fuel_weight=fuel_weight,
         )
 
+    def _get_diagnostics(self):
+        return self._gpu_inferno.get_diagnostics()
+
     def release(self):
         self._gpu_inferno.release()
+
+
+class GPUConsAvgBAModel(GPUBAModel):
+    def __init__(self, **kwargs):
+        logger.info("GPUConsAvgBAModel init.")
+        super().__init__(**kwargs)
+
+        # NOTE This is a workaround based on the assumption that cropland fraction is
+        # slowly changing, meaning that there should be little difference between
+        # averaging from original timesteps to 12 months, vs. aggregated timesteps to
+        # 12 months (as is being done below). This is done for computational
+        # convenience at this point.
+        assert not np.any(np.ma.getmaskarray(self.obs_pftcrop_1d.mask))
+        self.obs_pftcrop_1d = self._cons_monthly_avg.cons_monthly_average_data(
+            np.ma.getdata(self.obs_pftcrop_1d)
+        )
+        assert self.obs_pftcrop_1d.shape == (12, land_pts)
+
+    @property
+    def _gpu_class(self):
+        from .py_gpu_inferno import GPUInfernoAvg
+
+        return partial(GPUInfernoAvg, weights=self._cons_monthly_avg.weights)
 
 
 def gen_to_optimise(

src/python_inferno/multi_timestep_inferno.py
@@ -17,6 +17,50 @@ set_num_threads(get_ncpus())
 
 
 @njit(nogil=True, parallel=True, cache=True, fastmath=True)
+def _get_diagnostics(
+    *,
+    t1p5m_tile,
+    q1p5m_tile,
+    pstar,
+    ls_rain,
+    con_rain,
+):
+    Nt = pstar.shape[0]
+
+    # Output arrays.
+    inferno_rain_out = np.zeros((Nt, land_pts), dtype=np.float32)
+    qsat_out = np.zeros((Nt, npft, land_pts), dtype=np.float32)
+    inferno_rhum_out = np.zeros((Nt, npft, land_pts), dtype=np.float32)
+
+    # Tolerance number to filter non-physical rain values
+    rain_tolerance = 1.0e-18  # kg/m2/s
+
+    for l in prange(0, land_pts):
+        for ti in range(Nt):
+            ls_rain_filtered = ls_rain[ti, l]
+            con_rain_filtered = con_rain[ti, l]
+
+            if ls_rain_filtered < rain_tolerance:
+                ls_rain_filtered = 0.0
+            if con_rain_filtered < rain_tolerance:
+                con_rain_filtered = 0.0
+
+            inferno_rain = ls_rain_filtered + con_rain_filtered
+
+            inferno_rain_out[ti, l] = inferno_rain
+
+            for i in range(npft):
+                # Get the tile relative humidity using saturation routine
+                qsat = qsat_wat(t1p5m_tile[ti, i, l], pstar[ti, l])
+                qsat_out[ti, i, l] = qsat
+
+                inferno_rhum = (q1p5m_tile[ti, i, l] / qsat) * 100.0
+                inferno_rhum_out[ti, i, l] = inferno_rhum
+
+    return inferno_rain_out, qsat_out, inferno_rhum_out
+
+
+@njit(nogil=True, parallel=True, cache=True, fastmath=True)
 def _multi_timestep_inferno(
     *,
     t1p5m_tile,
@@ -86,6 +130,9 @@ def _multi_timestep_inferno(
 
     # Store the output BA (averaged over PFTs).
     burnt_area = np.zeros((Nt, land_pts))
+
+    # Ignore mask stored for diagnostic purposes.
+    checks_failed_mask = np.ones((Nt, npft, land_pts), dtype=np.bool_)
 
     # Plant Material that is available as fuel (on the surface)
     pmtofuel = 0.7
@@ -173,6 +220,8 @@ def _multi_timestep_inferno(
                     continue
 
                 # If all these checks are passes, start fire calculations
+                checks_failed_mask[ti, i, l] = False  # Record the fact that the
+                # checks have passed.
 
                 ignitions = calc_ignitions(
                     pop_den[l],
@@ -214,9 +263,9 @@ def _multi_timestep_inferno(
                     litter_pool_shape[pft_group_i],
                     include_temperature,
                     fapar_weight[pft_group_i],
-                    fuel_weight[pft_group_i],
-                    temperature_weight[pft_group_i],
                     dryness_weight[pft_group_i],
+                    temperature_weight[pft_group_i],
+                    fuel_weight[pft_group_i],
                 )
 
                 burnt_area_ft = calc_burnt_area(flammability_ft, ignitions, avg_ba[i])
@@ -224,7 +273,7 @@ def _multi_timestep_inferno(
                 # We add pft-specific variables to the gridbox totals
                 burnt_area[ti, l] += frac[ti, i, l] * burnt_area_ft
 
-    return burnt_area
+    return burnt_area, checks_failed_mask
 
 
 def multi_timestep_inferno(
@@ -276,6 +325,7 @@ def multi_timestep_inferno(
     temperature_weight,
     fuel_weight,
     land_point=-1,
+    return_checks_failed_mask=False,
 ):
     param_vars = dict(
         fapar_factor=fapar_factor,
@@ -311,7 +361,7 @@ def multi_timestep_inferno(
         transformed_param_vars[name] = np.asarray(val, dtype=np.float64)
         assert transformed_param_vars[name].shape == (N_pft_groups,)
 
-    ba = overall_scale * transform_dtype(_multi_timestep_inferno)(
+    raw_ba, checks_failed_mask = transform_dtype(_multi_timestep_inferno)(
         t1p5m_tile=t1p5m_tile,
         q1p5m_tile=q1p5m_tile,
         pstar=pstar,
@@ -337,4 +387,7 @@ def multi_timestep_inferno(
         land_point=land_point,
         **transformed_param_vars,
     )
+    ba = overall_scale * raw_ba
+    if return_checks_failed_mask:
+        return ba, checks_failed_mask
     return ba

src/python_inferno/py_gpu_inferno/__init__.py
@@ -9,6 +9,8 @@ from .py_gpu_inferno import GPUCalculatePhase
 from .py_gpu_inferno import GPUCompute as _GPUCompute
 from .py_gpu_inferno import GPUConsAvg as _GPUConsAvg
 from .py_gpu_inferno import GPUConsAvgNoMask as _GPUConsAvgNoMask
+from .py_gpu_inferno import GPUFlam2
+from .py_gpu_inferno import GPUInfernoAvg as _GPUInfernoAvg
 from .py_gpu_inferno import calculate_phase
 from .py_gpu_inferno import cons_avg_no_mask as _cons_avg_no_mask
 from .py_gpu_inferno import nme as _nme
@@ -41,8 +43,9 @@ class GPUInferno:
         grouped_dry_bal,
         litter_pool,
         dry_days,
+        checks_failed,
     ):
-        self.gpu_compute = _GPUCompute()
+        self.gpu_compute = self._get_compute()
         self.Nt = Nt
         self.frac = frac
         assert frac.shape == (self.Nt * n_total_pft * land_pts,)
@@ -71,8 +74,23 @@ class GPUInferno:
             grouped_dry_bal=grouped_dry_bal,
             litter_pool=litter_pool,
             dry_days=dry_days,
+            checks_failed=checks_failed,
         )
-        self.out = np.empty((self.Nt, land_pts), dtype=np.float32)
+        self.out = self._get_out_arr()
+
+    def _get_compute(self):
+        return _GPUCompute()
+
+    def get_checks_failed_mask(self):
+        return self.gpu_compute.get_checks_failed_mask().reshape(
+            self.Nt, npft, land_pts
+        )
+
+    def get_diagnostics(self):
+        return self.gpu_compute.get_diagnostics()
+
+    def _get_out_arr(self):
+        return np.empty((self.Nt, land_pts), dtype=np.float32)
 
     def run(
         self,
@@ -132,6 +150,20 @@ class GPUInferno:
         self.gpu_compute.release()
 
 
+class GPUInfernoAvg(GPUInferno):
+    def __init__(self, *args, weights, **kwargs):
+        weights[weights < 1e-9] = 0
+
+        self.weights = weights
+        super().__init__(*args, **kwargs)
+
+    def _get_compute(self):
+        return _GPUInfernoAvg(land_pts, self.weights.astype(np.float32))
+
+    def _get_out_arr(self):
+        return np.empty((12, land_pts), dtype=np.float32)
+
+
 class GPUCalculateMPD:
     def __init__(self, N):
         self.N = N
@@ -149,6 +181,8 @@ class GPUCalculateMPD:
 
 class GPUConsAvg:
     def __init__(self, L, weights):
+        weights[weights < 1e-9] = 0
+
         self.L = L
         self.M = weights.shape[0]
         self.N = weights.shape[1]
@@ -163,6 +197,8 @@ class GPUConsAvg:
 
 class GPUConsAvgNoMask:
     def __init__(self, L, weights):
+        weights[weights < 1e-9] = 0
+
         self.L = L
         self.M = weights.shape[0]
         self.N = weights.shape[1]

src/python_inferno/utils.py
@@ -347,9 +347,33 @@ class ConsMonthlyAvg:
     _compute_class = GPUConsAvg
 
     def __init__(self, time_coord, L):
-        self.L = L
-        self.trimmed, self.Nt, self.Nout, self.weights = calculate_weights(time_coord)
-        self.gpu_cons_avg = self._compute_class(L=L, weights=self.weights)
+        # Changing these properties will not automatically affect the underlying
+        # compute class.
+        self._L = L
+        self._trimmed, self._Nt, self._Nout, self._weights = calculate_weights(
+            time_coord
+        )
+        self.gpu_cons_avg = self._compute_class(L=self.L, weights=self.weights)
+
+    @property
+    def L(self):
+        return self._L
+
+    @property
+    def trimmed(self):
+        return self._trimmed
+
+    @property
+    def Nt(self):
+        return self._Nt
+
+    @property
+    def Nout(self):
+        return self._Nout
+
+    @property
+    def weights(self):
+        return self._weights
 
     def cons_monthly_average_data(self, data):
         if self.trimmed:
@@ -961,7 +985,7 @@ def transform_dtype(func):
     def _transform_dtype(**kwargs):
         out = {}
         for name, val in kwargs.items():
-            if isinstance(val, np.ndarray):
+            if isinstance(val, np.ndarray) and val.dtype != np.bool_:
                 out[name] = np.asarray(val, dtype=np.float32)
             else:
                 out[name] = val

tests/test_ba_model.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import pytest
+from numpy.testing import assert_allclose
 
-from python_inferno.ba_model import BAModel
+from python_inferno.ba_model import BAModel, GPUBAModel, GPUConsAvgBAModel
 from python_inferno.metrics import Metrics
 
 
@@ -24,6 +26,147 @@ def test_BAModel(params_model_ba):
             atol=1e-12,
             rtol=1e-7,
         )
+
+
+@pytest.mark.parametrize("param_index", range(4))
+@pytest.mark.parametrize("mod_index", range(100))
+def test_GPUBAModel(param_index, mod_index, params_model_ba):
+    rng = np.random.default_rng(mod_index)
+
+    params, expected_model_ba = list(params_model_ba)[param_index]
+
+    mod_params = {
+        # Modify parameters using random permutations.
+        **params,
+        **{
+            key: rng.random()
+            for key, val in (
+                ("fapar_weight", 1.0),
+                ("dryness_weight", 1.0),
+                ("temperature_weight", 1.0),
+                ("fuel_weight", 1.0),
+            )
+        },
+    }
+    mod_params["crop_f"] = rng.random()
+    mod_params["overall_scale"] = rng.random()
+
+    gpu_model = GPUBAModel(**mod_params)
+    gpu_ba = gpu_model.run(**mod_params)["model_ba"]
+
+    py_model = BAModel(**mod_params)
+    py_ba = py_model.run(**mod_params)["model_ba"]
+
+    assert_allclose(gpu_ba, py_ba, atol=3e-11, rtol=1.2e-5)
+
+    gpu_model.release()
+
+
+# Tests to test / benchmark combined ConsAvg class.
+
+
+@pytest.mark.parametrize("param_index", range(4))
+@pytest.mark.parametrize("mod_index", range(100))
+def test_GPUInferno_cons_avg(param_index, mod_index, params_model_ba):
+    rng = np.random.default_rng(mod_index)
+
+    params, expected_model_ba = list(params_model_ba)[param_index]
+
+    mod_params = {
+        # Modify parameters using random permutations.
+        **{
+            key: rng.random()
+            for key, val in (
+                ("fapar_weight", 1),
+                ("dryness_weight", 1),
+                ("temperature_weight", 1),
+                ("fuel_weight", 1),
+            )
+        },
+        **params,
+    }
+
+    gpu_cons_avg_model = GPUConsAvgBAModel(**mod_params)
+    gpu_cons_avg_ba = gpu_cons_avg_model.run(**mod_params)["model_ba"]
+
+    gpu_model = GPUBAModel(**mod_params)
+    gpu_ba = gpu_model.run(**mod_params)["model_ba"]
+    gpu_ba2 = gpu_model._cons_monthly_avg.cons_monthly_average_data(gpu_ba)
+
+    py_model = BAModel(**mod_params)
+    py_ba = py_model.run(**mod_params)["model_ba"]
+    py_ba2 = py_model._cons_monthly_avg.cons_monthly_average_data(py_ba)
+
+    # Compare un-averaged BA.
+    assert_allclose(py_ba, gpu_ba, atol=3e-11, rtol=1e-5)
+
+    # Compare averaged BA.
+    assert_allclose(gpu_ba2, py_ba2, atol=3e-11, rtol=1e-5)
+    assert_allclose(gpu_cons_avg_ba, gpu_ba2, atol=1e-10, rtol=1e-3)
+    assert_allclose(gpu_cons_avg_ba, py_ba2, atol=1e-10, rtol=1e-3)
+
+    gpu_cons_avg_model.release()
+    gpu_model.release()
+
+
+CONS_AVG_ITER = 10
+CONS_AVG_ROUNDS = 100
+
+
+@pytest.mark.parametrize("index", range(4))
+def test_GPUInferno_cons_avg_combined_benchmark(index, params_model_ba, benchmark):
+    params, expected_model_ba = list(params_model_ba)[index]
+    params = {
+        **dict(
+            fapar_weight=1,
+            dryness_weight=1,
+            temperature_weight=1,
+            fuel_weight=1,
+        ),
+        **params,
+    }
+
+    rng = np.random.default_rng(0)
+
+    model1 = GPUConsAvgBAModel(**params)
+
+    def _bench():
+        mod_params = params.copy()
+        mod_params["fapar_weight"] += rng.random()
+        return model1.run(**mod_params)
+
+    benchmark.pedantic(_bench, iterations=CONS_AVG_ITER, rounds=CONS_AVG_ROUNDS)
+
+    model1.release()
+
+
+@pytest.mark.parametrize("index", range(4))
+def test_GPUInferno_cons_avg_separate_benchmark(index, params_model_ba, benchmark):
+    params, expected_model_ba = list(params_model_ba)[index]
+    params = {
+        **dict(
+            fapar_weight=1,
+            dryness_weight=1,
+            temperature_weight=1,
+            fuel_weight=1,
+        ),
+        **params,
+    }
+
+    rng = np.random.default_rng(0)
+
+    model2 = GPUBAModel(**params)
+
+    def _bench():
+        mod_params = params.copy()
+        mod_params["fapar_weight"] += rng.random()
+        return model2._cons_monthly_avg.cons_monthly_average_data(
+            model2.run(**mod_params)["model_ba"]
+        )
+
+    benchmark.pedantic(_bench, iterations=CONS_AVG_ITER, rounds=CONS_AVG_ROUNDS)
+
+    model2.release()
 
 
 def test_calculate_scores_benchmark(benchmark, model_params):

tests/test_py_gpu_inferno.py
@@ -4,7 +4,7 @@ import numpy as np
 import pytest
 from numpy.testing import assert_allclose
 
-from python_inferno.ba_model import GPUBAModel
+from python_inferno.ba_model import BAModel, GPUBAModel, GPUConsAvgBAModel
 from python_inferno.configuration import (
     N_pft_groups,
     land_pts,
@@ -13,10 +13,13 @@ from python_inferno.configuration import (
     n_cell_no_pft,
     n_cell_tot_pft,
 )
+from python_inferno.inferno import calc_flam
 from python_inferno.py_gpu_inferno import (
     GPUCalculateMPD,
     GPUCalculatePhase,
+    GPUFlam2,
     _GPUCompute,
+    _GPUInfernoAvg,
     cpp_nme,
 )
 
@@ -60,6 +63,7 @@ def get_compute_data():
             grouped_dry_bal=np.zeros(n_cell_grp_pft(Nt), dtype=np.float32),
             litter_pool=np.zeros(n_cell_nat_pft(Nt), dtype=np.float32),
             dry_days=np.zeros(n_cell_no_pft(Nt), dtype=np.float32),
+            checks_failed=np.zeros(n_cell_nat_pft(Nt), dtype=np.bool_),
         )
 
     return _get_compute_data
@@ -130,39 +134,31 @@ def test_many_run(compute, get_compute_data, compute_params):
 
 
 def test_GPUBAModel(params_model_ba):
-    for params, expected_model_ba in params_model_ba:
+    for _params, expected_model_ba in params_model_ba:
+        rng = np.random.default_rng(0)
+
+        params = {
+            **dict(
+                fapar_weight=1,
+                dryness_weight=1,
+                temperature_weight=1,
+                fuel_weight=1,
+            ),
+            **_params,
+        }
+
+        rand_params = {key: rng.random(1) for key in params}
+
         # Set up the model.
         model = GPUBAModel(**params)
         # Initialise the parameters using random values to ensure that modifying the
         # parameters later on works as expected.
-        rng = np.random.default_rng(0)
-        random_model_ba = model.run(
-            **{
-                key: rng.random(1)
-                for key in (
-                    *params.keys(),
-                    "fapar_weight",
-                    "dryness_weight",
-                    "temperature_weight",
-                    "fuel_weight",
-                )
-            }
-        )["model_ba"]
+        random_model_ba = model.run(**rand_params)["model_ba"]
         assert not np.allclose(
             random_model_ba, expected_model_ba["metal"], atol=1e-12, rtol=1e-7
         )
         # Set the proper parameters and run.
-        model_ba = model.run(
-            **{
-                **dict(
-                    fapar_weight=1,
-                    dryness_weight=1,
-                    temperature_weight=1,
-                    fuel_weight=1,
-                ),
-                **params,
-            }
-        )["model_ba"]
+        model_ba = model.run(**params)["model_ba"]
         assert np.allclose(model_ba, expected_model_ba["metal"], atol=1e-12, rtol=1e-7)
 
         model.release()
@@ -236,3 +232,216 @@ def test_cpp_nme():
         )
         > 0
     )
+
+
+@pytest.mark.parametrize("includeTemperature", [0, 1])
+@pytest.mark.parametrize("fuelBuildUpMethod", [1, 2])
+@pytest.mark.parametrize("drynessMethod", [1, 2])
+@pytest.mark.parametrize("Nt", [100, 10])
+def test_GPUInfernoConsAvg_synthetic(
+    get_compute_data,
+    compute_params,
+    includeTemperature,
+    fuelBuildUpMethod,
+    drynessMethod,
+    Nt,
+):
+    compute = _GPUInfernoAvg(
+        land_pts,
+        np.random.default_rng(0).random((Nt, 12), dtype=np.float32),
+    )
+    compute.set_data(
+        **get_compute_data(
+            includeTemperature=includeTemperature,
+            fuelBuildUpMethod=fuelBuildUpMethod,
+            drynessMethod=drynessMethod,
+            Nt=Nt,
+        )
+    )
+    compute.set_params(**compute_params)
+    compute.run(np.empty((12, land_pts), dtype=np.float32))
+    compute.release()
+
+
+def test_GPUInfernoConsAvg(params_model_ba):
+    for _params, expected_model_ba in params_model_ba:
+        params = {
+            **dict(
+                fapar_weight=1,
+                dryness_weight=1,
+                temperature_weight=1,
+                fuel_weight=1,
+            ),
+            **_params,
+        }
+
+        # Set up the model.
+        model = GPUConsAvgBAModel(**params)
+
+        test_ba = model.run(**params)["model_ba"]
+
+        # Carry out monthly averaging of the expected output to compare to the already
+        # averaged output above.
+        expected = model._cons_monthly_avg.cons_monthly_average_data(
+            expected_model_ba["metal"]
+        )
+
+        assert_allclose(test_ba, expected, atol=1e-12, rtol=1e-3)
+
+        model.release()
+
+
+def test_flam():
+    compute = GPUFlam2()
+
+    def gpu_calc_flam(**params):
+        return compute.run(
+            temp_l=params["temp_l"],
+            fuel_build_up=params["fuel_build_up"],
+            fapar=params["fapar"],
+            dry_days=params["dry_days"],
+            dryness_method=params["dryness_method"],
+            fuel_build_up_method=params["fuel_build_up_method"],
+            fapar_factor=params["fapar_factor"],
+            fapar_centre=params["fapar_centre"],
+            fapar_shape=params["fapar_shape"],
+            fuel_build_up_factor=params["fuel_build_up_factor"],
+            fuel_build_up_centre=params["fuel_build_up_centre"],
+            fuel_build_up_shape=params["fuel_build_up_shape"],
+            temperature_factor=params["temperature_factor"],
+            temperature_centre=params["temperature_centre"],
+            temperature_shape=params["temperature_shape"],
+            dry_day_factor=params["dry_day_factor"],
+            dry_day_centre=params["dry_day_centre"],
+            dry_day_shape=params["dry_day_shape"],
+            dry_bal=params["dry_bal"],
+            dry_bal_factor=params["dry_bal_factor"],
+            dry_bal_centre=params["dry_bal_centre"],
+            dry_bal_shape=params["dry_bal_shape"],
+            litter_pool=params["litter_pool"],
+            litter_pool_factor=params["litter_pool_factor"],
+            litter_pool_centre=params["litter_pool_centre"],
+            litter_pool_shape=params["litter_pool_shape"],
+            include_temperature=params["include_temperature"],
+            fapar_weight=params["fapar_weight"],
+            dryness_weight=params["dryness_weight"],
+            temperature_weight=params["temperature_weight"],
+            fuel_weight=params["fuel_weight"],
+        )
+
+    for seed in range(10000):
+        rng = np.random.default_rng(seed)
+
+        # Use random data and parameters to simulate variability of real nputs.
+        params = dict(
+            temp_l=rng.random(),
+            rhum_l=0.0,
+            fuel_l=0.0,
+            sm_l=0.0,
+            rain_l=0.0,
+            fuel_build_up=rng.random(),
+            fapar=rng.random(),
+            dry_days=rng.random(),
+            flammability_method=2,
+            dryness_method=rng.integers(1, 3),
+            fuel_build_up_method=rng.integers(1, 3),
+            fapar_factor=rng.random(),
+            fapar_centre=rng.random(),
+            fapar_shape=rng.random(),
+            fuel_build_up_factor=rng.random(),
+            fuel_build_up_centre=rng.random(),
+            fuel_build_up_shape=rng.random(),
+            temperature_factor=rng.random(),
+            temperature_centre=rng.random(),
+            temperature_shape=rng.random(),
+            dry_day_factor=rng.random(),
+            dry_day_centre=rng.random(),
+            dry_day_shape=rng.random(),
+            dry_bal=rng.random(),
+            dry_bal_factor=rng.random(),
+            dry_bal_centre=rng.random(),
+            dry_bal_shape=rng.random(),
+            litter_pool=rng.random(),
+            litter_pool_factor=rng.random(),
+            litter_pool_centre=rng.random(),
+            litter_pool_shape=rng.random(),
+            include_temperature=rng.integers(2),
+            fapar_weight=rng.random(),
+            dryness_weight=rng.random(),
+            temperature_weight=rng.random(),
+            fuel_weight=rng.random(),
+        )
+
+        python_flam = calc_flam(**params)
+        metal_flam = gpu_calc_flam(**params)
+
+        assert_allclose(python_flam, metal_flam, atol=1e-8, rtol=1e-4)
+
+    # Test 0 weights case.
+    for key in (
+        "fapar_weight",
+        "dryness_weight",
+        "temperature_weight",
+        "fuel_weight",
+    ):
+        params[key] = 0.0
+
+    python_flam = calc_flam(**params)
+    metal_flam = gpu_calc_flam(**params)
+
+    assert_allclose(python_flam, 1, atol=1e-8, rtol=1e-4)
+    assert_allclose(metal_flam, 1, atol=1e-8, rtol=1e-4)
+
+    compute.release()
+
+
+def test_checks_mask(model_params):
+    for _, _params in model_params.items():
+        params = {
+            **dict(
+                fapar_weight=1,
+                dryness_weight=1,
+                temperature_weight=1,
+                fuel_weight=1,
+            ),
+            **_params,
+        }
+
+        metal_model = GPUBAModel(**params)
+        mask = metal_model._gpu_inferno.get_checks_failed_mask()
+
+        python_model = BAModel(**params)
+        expected = python_model._get_checks_failed_mask(**params)
+
+        assert np.all(mask == expected)
+
+        metal_model.release()
+
+
+def test_diagnostics(model_params):
+    for _, _params in model_params.items():
+        params = {
+            **dict(
+                fapar_weight=1,
+                dryness_weight=1,
+                temperature_weight=1,
+                fuel_weight=1,
+            ),
+            **_params,
+        }
+
+        metal_model = GPUBAModel(**params)
+        metal_diagnostics = metal_model._get_diagnostics()
+
+        python_model = BAModel(**params)
+        python_diagnostics = python_model._get_diagnostics()
+
+        assert len(metal_diagnostics) == len(python_diagnostics) == 3
+
+        for i in range(len(metal_diagnostics)):
+            # NOTE This very high tolerance probably indicates the unreliability of
+            # certain floating point calculations. Therefore, the Python (Numba)
+            # version should be preferred.
+            assert_allclose(metal_diagnostics[i], python_diagnostics[i], rtol=0.6)
+
+        metal_model.release()