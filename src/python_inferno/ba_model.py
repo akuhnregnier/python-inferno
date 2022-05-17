Used GPUConsAvgScoreBAModel for optimisation. Fixed optimised weights to intended [0, 1] range. Refactored Python-side check mask calculation to not require any extraneous data or information. Assumed no malformed parameters are passed to multi_timestep_inferno function.

src/python_inferno/ba_model.py
@@ -7,9 +7,13 @@ from loguru import logger
 from sklearn.metrics import r2_score
 
 from .configuration import N_pft_groups, land_pts, npft
-from .data import get_processed_climatological_data, timestep
+from .data import get_processed_climatological_data
 from .metrics import Metrics, loghist, nmse
-from .multi_timestep_inferno import _get_diagnostics, multi_timestep_inferno
+from .multi_timestep_inferno import (
+    _get_checks_failed_mask,
+    _get_diagnostics,
+    multi_timestep_inferno,
+)
 from .py_gpu_inferno import GPUCalculateMPD, cpp_nme
 from .utils import (
     ConsMonthlyAvgNoMask,
@@ -68,7 +72,6 @@ def run_model(
 ):
     return unpack_wrapped(multi_timestep_inferno)(
         ignition_method=1,
-        timestep=timestep,
         flammability_method=2,
         dryness_method=dryness_method,
         fuel_build_up_method=fuel_build_up_method,
@@ -84,6 +87,12 @@ def run_model(
     )
 
 
+def mpd_check(mpd_ignored):
+    if mpd_ignored > MPD_IGNORE_THRES:
+        # Ensure that not too many samples are ignored.
+        raise BAModelException()
+
+
 def calculate_mpd(avg_ba, mon_avg_gfed_ba_1d, mpd=GPUCalculateMPD(land_pts).run):
     if mon_avg_gfed_ba_1d.shape[0] == 12:
         pad_func = lambda x: x
@@ -96,9 +105,7 @@ def calculate_mpd(avg_ba, mon_avg_gfed_ba_1d, mpd=GPUCalculateMPD(land_pts).run)
     mpd_val, ignored = mpd(
         obs=pad_func(mon_avg_gfed_ba_1d), pred=pad_func(avg_ba), return_ignored=True
     )
-    if ignored > MPD_IGNORE_THRES:
-        # Ensure that not too many samples are ignored.
-        raise BAModelException()
+    mpd_check(ignored)
 
     return mpd_val, ignored
 
@@ -376,23 +383,15 @@ class BAModel:
             **processed_kwargs,
         )
 
-    def _get_checks_failed_mask(
-        self,
-        *,
-        crop_f,
-        **kwargs,
-    ):
-        processed_kwargs = self.process_kwargs(**kwargs)
-        _, checks_failed_mask = BAModel.get_model_ba(
-            self,
-            dryness_method=self.dryness_method,
-            fuel_build_up_method=self.fuel_build_up_method,
-            include_temperature=self.include_temperature,
-            data_dict=self.data_dict,
-            return_checks_failed_mask=True,
-            **processed_kwargs,
+    def _get_checks_failed_mask(self):
+        return transform_dtype(_get_checks_failed_mask)(
+            t1p5m_tile=self.data_dict["t1p5m_tile"],
+            q1p5m_tile=self.data_dict["q1p5m_tile"],
+            pstar=self.data_dict["pstar"],
+            sthu_soilt_single=self.data_dict["sthu_soilt_single"],
+            ls_rain=self.data_dict["ls_rain"],
+            con_rain=self.data_dict["con_rain"],
         )
-        return checks_failed_mask
 
     def _get_diagnostics(self):
         return _get_diagnostics(
@@ -487,7 +486,7 @@ class GPUBAModel(BAModel):
             grouped_dry_bal=self.data_dict["grouped_dry_bal"].ravel(),
             litter_pool=self.data_dict["litter_pool"].ravel(),
             dry_days=self.data_dict["dry_days"].ravel(),
-            checks_failed=BAModel._get_checks_failed_mask(self, **kwargs),
+            checks_failed=BAModel._get_checks_failed_mask(self),
         )
 
     @property
@@ -649,30 +648,43 @@ def gen_to_optimise(
     success_func,
     **model_ba_init_kwargs,
 ):
+    requested = (Metrics.MPD, Metrics.ARCSINH_NME)
     try:
-        ba_model = GPUBAModel(**model_ba_init_kwargs)
+        score_model = GPUConsAvgScoreBAModel(**model_ba_init_kwargs)
+
+        def to_optimise(**run_kwargs):
+            scores = score_model.get_scores(requested=requested, **run_kwargs)
+
+            try:
+                mpd_check(scores["mpd_ignored"])
+            except BAModelException:
+                return fail_func()
+
+            # Aim to minimise the combined score.
+            loss = scores["arcsinh_nme"] + scores["mpd"]
+            if np.isnan(loss):
+                return fail_func()
+
+            return success_func(loss)
+
     except ModuleNotFoundError:
         logger.warning("GPU INFERNO module not found.")
         ba_model = BAModel(**model_ba_init_kwargs)
 
-    def to_optimise(**run_kwargs):
-        # NOTE: It is assumed here that the data being operated on will not change
-        # between runs of the `to_optimise` function. Data-relevant parameters in
-        # `run_kwargs` will be silently ignored!
-        try:
-            model_ba = ba_model.run(**run_kwargs)["model_ba"]
-            scores = ba_model.calc_scores(
-                model_ba=model_ba, requested=(Metrics.MPD, Metrics.ARCSINH_NME)
-            )["scores"]
-        except BAModelException:
-            return fail_func()
+        def to_optimise(**run_kwargs):
+            try:
+                model_ba = ba_model.run(**run_kwargs)["model_ba"]
+                scores = ba_model.calc_scores(model_ba=model_ba, requested=requested)[
+                    "scores"
+                ]
+            except BAModelException:
+                return fail_func()
 
-        # Aim to minimise the combined score.
-        # loss = scores["nme"] + scores["nmse"] + scores["mpd"] + 2 * scores["loghist"]
-        loss = scores["arcsinh_nme"] + scores["mpd"]
-        logger.debug(f"loss: {loss:0.6f}")
-        return success_func(loss)
+            # Aim to minimise the combined score.
+            loss = scores["arcsinh_nme"] + scores["mpd"]
+            return success_func(loss)
 
-    # TODO Call release on GPU class when done?
-
+    # NOTE: It is assumed here that the data being operated on will not change
+    # between runs of the `to_optimise` function. Data-relevant parameters in
+    # `run_kwargs` will be silently ignored!
     return to_optimise

src/python_inferno/multi_timestep_inferno.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 import numpy as np
-from loguru import logger
 from numba import njit, prange, set_num_threads
 from wildfires.qstat import get_ncpus
 
 from .calc_c_comps_triffid import calc_c_comps_triffid
-from .configuration import N_pft_groups, avg_ba, land_pts, npft
+from .configuration import avg_ba, land_pts, npft
 from .inferno import calc_burnt_area, calc_flam, calc_ignitions
 from .qsat_wat import qsat_wat
 from .utils import get_pft_group_index, transform_dtype
@@ -58,6 +57,90 @@ def _get_diagnostics(
                 inferno_rhum_out[ti, i, l] = inferno_rhum
 
     return inferno_rain_out, qsat_out, inferno_rhum_out
+
+
+@njit(nogil=True, parallel=True, cache=True, fastmath=True)
+def _get_checks_failed_mask(
+    *,
+    t1p5m_tile,
+    q1p5m_tile,
+    pstar,
+    sthu_soilt_single,
+    ls_rain,
+    con_rain,
+):
+    # Ensure consistency of the time dimension.
+    if not (
+        t1p5m_tile.shape[0]
+        == q1p5m_tile.shape[0]
+        == pstar.shape[0]
+        == sthu_soilt_single.shape[0]
+        == ls_rain.shape[0]
+        == con_rain.shape[0]
+    ):
+        raise ValueError("All arrays need to have the same time dimension.")
+
+    Nt = pstar.shape[0]
+
+    # Ignore mask stored for diagnostic purposes.
+    checks_failed_mask = np.ones((Nt, npft, land_pts), dtype=np.bool_)
+
+    # Tolerance number to filter non-physical rain values
+    rain_tolerance = 1.0e-18  # kg/m2/s
+
+    # Soil Humidity (inferno_sm)
+    inferno_sm = sthu_soilt_single
+
+    for l in prange(land_pts):
+        for ti in range(Nt):
+            # Rainfall (inferno_rain)
+
+            # Rain fall values have a significant impact in the calculation of flammability.
+            # In some cases we may be presented with values that have no significant meaning -
+            # e.g in the UM context negative values or very small values can often be found/
+
+            ls_rain_filtered = ls_rain[ti, l]
+            con_rain_filtered = con_rain[ti, l]
+
+            if ls_rain_filtered < rain_tolerance:
+                ls_rain_filtered = 0.0
+            if con_rain_filtered < rain_tolerance:
+                con_rain_filtered = 0.0
+
+            inferno_rain = ls_rain_filtered + con_rain_filtered
+
+            # The maximum rain rate ever observed is 38mm in one minute,
+            # here we assume 0.5mm/s stops fires altogether
+            if (inferno_rain > 0.5) or (inferno_rain < 0.0):
+                continue
+
+            # Soil moisture is a fraction of saturation
+            if (inferno_sm[ti, l] > 1.0) or (inferno_sm[ti, l] < 0.0):
+                continue
+
+            for i in range(npft):
+                # Conditional statements to make sure we are dealing with
+                # reasonable weather. Note initialisation to 0 already done.
+                # If the driving variables are singularities, we assume
+                # no burnt area.
+
+                # Temperatures constrained akin to qsat (from the WMO)
+                if (t1p5m_tile[ti, i, l] > 338.15) or (t1p5m_tile[ti, i, l] < 183.15):
+                    continue
+
+                # Get the tile relative humidity using saturation routine
+                qsat = qsat_wat(t1p5m_tile[ti, i, l], pstar[ti, l])
+
+                inferno_rhum = (q1p5m_tile[ti, i, l] / qsat) * 100.0
+
+                # Relative Humidity should be constrained to 0-100
+                if (inferno_rhum > 100.0) or (inferno_rhum < 0.0):
+                    continue
+
+                # Record the fact that the checks have passed.
+                checks_failed_mask[ti, i, l] = False
+
+    return checks_failed_mask
 
 
 @njit(nogil=True, parallel=True, cache=True, fastmath=True)
@@ -130,9 +213,6 @@ def _multi_timestep_inferno(
 
     # Store the output BA (averaged over PFTs).
     burnt_area = np.zeros((Nt, land_pts))
-
-    # Ignore mask stored for diagnostic purposes.
-    checks_failed_mask = np.ones((Nt, npft, land_pts), dtype=np.bool_)
 
     # Plant Material that is available as fuel (on the surface)
     pmtofuel = 0.7
@@ -219,8 +299,6 @@ def _multi_timestep_inferno(
                     continue
 
                 # If all these checks are passes, start fire calculations
-                checks_failed_mask[ti, i, l] = False  # Record the fact that the
-                # checks have passed.
 
                 ignitions = calc_ignitions(
                     pop_den[l],
@@ -272,121 +350,17 @@ def _multi_timestep_inferno(
                 # We add pft-specific variables to the gridbox totals
                 burnt_area[ti, l] += frac[ti, i, l] * burnt_area_ft
 
-    return burnt_area, checks_failed_mask
+    return burnt_area
 
 
 def multi_timestep_inferno(
     *,
-    t1p5m_tile,
-    q1p5m_tile,
-    pstar,
-    sthu_soilt_single,
-    frac,
-    c_soil_dpm_gb,
-    c_soil_rpm_gb,
-    canht,
-    ls_rain,
-    con_rain,
-    pop_den,
-    flash_rate,
-    ignition_method,
-    fuel_build_up,
-    fapar_diag_pft,
-    grouped_dry_bal,
-    dry_days,
-    litter_pool,
-    fapar_factor,
-    fapar_centre,
-    fapar_shape,
-    fuel_build_up_factor,
-    fuel_build_up_centre,
-    fuel_build_up_shape,
-    temperature_factor,
-    temperature_centre,
-    temperature_shape,
-    litter_pool_factor,
-    litter_pool_centre,
-    litter_pool_shape,
-    flammability_method,
-    dryness_method,
-    fuel_build_up_method,
-    dry_day_factor,
-    dry_day_centre,
-    dry_day_shape,
-    dry_bal_factor,
-    dry_bal_centre,
-    dry_bal_shape,
-    include_temperature,
-    timestep,
     overall_scale,
-    fapar_weight,
-    dryness_weight,
-    temperature_weight,
-    fuel_weight,
     land_point=-1,
-    return_checks_failed_mask=False,
+    **kwargs,
 ):
-    param_vars = dict(
-        fapar_factor=fapar_factor,
-        fapar_centre=fapar_centre,
-        fapar_shape=fapar_shape,
-        fuel_build_up_factor=fuel_build_up_factor,
-        fuel_build_up_centre=fuel_build_up_centre,
-        fuel_build_up_shape=fuel_build_up_shape,
-        temperature_factor=temperature_factor,
-        temperature_centre=temperature_centre,
-        temperature_shape=temperature_shape,
-        dry_day_factor=dry_day_factor,
-        dry_day_centre=dry_day_centre,
-        dry_day_shape=dry_day_shape,
-        dry_bal_factor=dry_bal_factor,
-        dry_bal_centre=dry_bal_centre,
-        dry_bal_shape=dry_bal_shape,
-        litter_pool_factor=litter_pool_factor,
-        litter_pool_centre=litter_pool_centre,
-        litter_pool_shape=litter_pool_shape,
-        fapar_weight=fapar_weight,
-        dryness_weight=dryness_weight,
-        temperature_weight=temperature_weight,
-        fuel_weight=fuel_weight,
-    )
-
-    # Ensure the parameters are given as arrays with `N_pft_groups` elements.
-    transformed_param_vars = dict()
-    for name, val in param_vars.items():
-        if not hasattr(val, "__iter__"):
-            logger.debug(f"Duplicating: {name}")
-            val = [val] * N_pft_groups
-        transformed_param_vars[name] = np.asarray(val, dtype=np.float64)
-        assert transformed_param_vars[name].shape == (N_pft_groups,)
-
-    raw_ba, checks_failed_mask = transform_dtype(_multi_timestep_inferno)(
-        t1p5m_tile=t1p5m_tile,
-        q1p5m_tile=q1p5m_tile,
-        pstar=pstar,
-        sthu_soilt_single=sthu_soilt_single,
-        frac=frac,
-        c_soil_dpm_gb=c_soil_dpm_gb,
-        c_soil_rpm_gb=c_soil_rpm_gb,
-        canht=canht,
-        ls_rain=ls_rain,
-        con_rain=con_rain,
-        pop_den=pop_den,
-        flash_rate=flash_rate,
-        ignition_method=int(ignition_method),
-        fuel_build_up=fuel_build_up,
-        fapar_diag_pft=fapar_diag_pft,
-        grouped_dry_bal=grouped_dry_bal,
-        dry_days=dry_days,
-        flammability_method=int(flammability_method),
-        dryness_method=int(dryness_method),
-        litter_pool=litter_pool,
-        fuel_build_up_method=int(fuel_build_up_method),
-        include_temperature=int(include_temperature),
+    raw_ba = transform_dtype(_multi_timestep_inferno)(
         land_point=land_point,
-        **transformed_param_vars,
+        **kwargs,
     )
-    ba = overall_scale * raw_ba
-    if return_checks_failed_mask:
-        return ba, checks_failed_mask
-    return ba
+    return overall_scale * raw_ba

tests/test_py_gpu_inferno.py
@@ -428,7 +428,7 @@ def test_checks_mask(model_params):
         mask = metal_model._gpu_inferno.get_checks_failed_mask()
 
         python_model = BAModel(**params)
-        expected = python_model._get_checks_failed_mask(**params)
+        expected = python_model._get_checks_failed_mask()
 
         assert np.all(mask == expected)
 
@@ -478,6 +478,8 @@ def test_GPUInfernoConsAvgScore(index, seed, model_params):
             dryness_weight=rng.random(),
             temperature_weight=rng.random(),
             fuel_weight=rng.random(),
+            overall_scale=100 * rng.random(),
+            crop_f=rng.random(),
         ),
     }
 