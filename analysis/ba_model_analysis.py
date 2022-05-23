Updated ba_model_analysis script for new BAModel class. Removed low BA mask to match optimisation. Fixed random seed for resampled error calculation. Fixed mpd function to maintain a constant dependency hash (function as default argument causes hash to vary between runs).

analysis/ba_model_analysis.py
@@ -15,18 +15,18 @@ from jules_output_analysis.data import cube_1d_to_2d, get_1d_data_cube
 from loguru import logger
 from tqdm import tqdm
 
-from python_inferno.ba_model import BAModel, Status, calculate_scores
+from python_inferno.ba_model import BAModel, calculate_scores
 from python_inferno.cache import cache
 from python_inferno.configuration import land_pts
 from python_inferno.data import load_data, load_jules_lats_lons
-from python_inferno.metrics import null_model_analysis
+from python_inferno.metrics import Metrics
+from python_inferno.metrics_plotting import null_model_analysis
 from python_inferno.model_params import get_model_params
 from python_inferno.plotting import plotting
 from python_inferno.utils import (
     ConsMonthlyAvg,
     PartialDateTime,
     get_apply_mask,
-    get_ba_mask,
     memoize,
     temporal_processing,
 )
@@ -129,13 +129,14 @@ if __name__ == "__main__":
     logger.remove()
     logger.add(sys.stderr, level="INFO")
 
+    requested = (Metrics.MPD, Metrics.ARCSINH_NME)
+
     jules_lats, jules_lons = load_jules_lats_lons()
 
     # To prevent memory accumulation during repeated calculations below.
     memoize.active = False
 
-    # XXX - 'opt_record_bak' vs. 'opt_record'
-    record_dir = Path(os.environ["EPHEMERAL"]) / "opt_record_bak"
+    record_dir = Path(os.environ["EPHEMERAL"]) / "opt_record"
     df, method_iter = get_model_params(
         record_dir=record_dir, progress=True, verbose=True
     )
@@ -174,34 +175,33 @@ if __name__ == "__main__":
         model_ba, mon_avg_gfed_ba_1d = itemgetter("model_ba", "mon_avg_gfed_ba_1d")(
             ba_model.run(**params)
         )
-        scores, calc_factors = itemgetter("scores", "calc_factors")(
-            ba_model.score(model_ba=model_ba)
+        scores, avg_ba = itemgetter("scores", "avg_ba")(
+            ba_model.calc_scores(model_ba=model_ba, requested=requested)
         )
 
-        model_ba *= calc_factors["adj_factor"]
-
-        ba_mask_1d = get_ba_mask(mon_avg_gfed_ba_1d)
-        apply_ba_mask_1d = get_apply_mask(ba_mask_1d)
+        # NOTE Low BA mask is not used during the optimisation!
+        # ba_mask_1d = get_ba_mask(mon_avg_gfed_ba_1d)
+        # apply_ba_mask_1d = get_apply_mask(ba_mask_1d)
 
         orig_mon_avg_gfed_ba_1d = mon_avg_gfed_ba_1d
-        mon_avg_gfed_ba_1d = apply_ba_mask_1d(mon_avg_gfed_ba_1d)
-        model_ba = apply_ba_mask_1d(model_ba)
+        # mon_avg_gfed_ba_1d = apply_ba_mask_1d(mon_avg_gfed_ba_1d)
+        # model_ba = apply_ba_mask_1d(model_ba)  # ??
+        # avg_ba = apply_ba_mask_1d(avg_ba)  # ??
 
         gc.collect()
 
-        model_ba_1d = get_1d_data_cube(model_ba, lats=jules_lats, lons=jules_lons)
         logger.info("Getting 2D cube")
-        model_ba_2d = cube_1d_to_2d(model_ba_1d)
+        model_ba_2d = cube_1d_to_2d(
+            get_1d_data_cube(avg_ba, lats=jules_lats, lons=jules_lons)
+        )
 
         gc.collect()
 
         plot_data[exp_name] = dict(
             exp_key=exp_key,
-            raw_data=np.ma.getdata(model_ba)[~np.ma.getmaskarray(model_ba)],
+            raw_data=np.ma.getdata(avg_ba)[~np.ma.getmaskarray(avg_ba)],
             model_ba_2d_data=model_ba_2d.data,
             hist_bins=hist_bins,
-            arcsinh_adj_factor=calc_factors["arcsinh_adj_factor"],
-            arcsinh_factor=calc_factors["arcsinh_factor"],
             scores=scores,
             data_params=params,
         )
@@ -223,24 +223,17 @@ if __name__ == "__main__":
         ],
         model_ba_2d_data=reference_obs,
         hist_bins=hist_bins,
-        # NOTE: Assuming arcsinh_factor is constant across all experiments, which
-        # should be true.
-        arcsinh_adj_factor=1.0,
-        arcsinh_factor=calc_factors["arcsinh_factor"],
     )
     plot_prog.update()
 
     # Old INFERNO BA.
     data_dict, jules_time_coord = get_processed_climatological_jules_ba()
     jules_ba_gb = data_dict.pop("jules_ba_gb")
-    scores, status, avg_jules_ba, calc_factors = calculate_scores(
+    scores, avg_jules_ba = calculate_scores(
         model_ba=jules_ba_gb,
         cons_monthly_avg=ConsMonthlyAvg(jules_time_coord, L=land_pts),
         mon_avg_gfed_ba_1d=mon_avg_gfed_ba_1d,
     )
-    assert status is Status.SUCCESS, "Score calculation failed!"
-
-    avg_jules_ba *= calc_factors["adj_factor"]
 
     plot_data["Old INFERNO BA"] = dict(
         raw_data=np.ma.getdata(avg_jules_ba)[~np.ma.getmaskarray(avg_jules_ba)],
@@ -250,8 +243,6 @@ if __name__ == "__main__":
             ).data
         ),
         hist_bins=hist_bins,
-        arcsinh_adj_factor=calc_factors["arcsinh_adj_factor"],
-        arcsinh_factor=calc_factors["arcsinh_factor"],
         scores=scores,
         # TODO Which params to use (combination of all?) - call again with actual
         # old INFERNO params to get corresponding data - is this supported?
@@ -270,7 +261,6 @@ if __name__ == "__main__":
             )
         )
 
-    # TODO For error calculations, do not use the version with the low BA mask?
     orig_reference_obs = cube_1d_to_2d(
         get_1d_data_cube(orig_mon_avg_gfed_ba_1d, lats=jules_lats, lons=jules_lons)
     ).data

src/python_inferno/metrics.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
-import math
 from enum import Enum
-from string import ascii_lowercase
 
-import matplotlib.pyplot as plt
 import numpy as np
 from loguru import logger
 from numba import njit
@@ -12,7 +9,6 @@ from sklearn.metrics import r2_score
 from tqdm import tqdm
 
 from .cache import cache, mark_dependency
-from .data import get_gfed_regions, get_pnv_mega_plot_data
 from .utils import linspace_no_endpoint
 
 Metrics = Enum("Metrics", ["NME", "NMSE", "MPD", "R2", "LOGHIST", "ARCSINH_NME"])
@@ -96,9 +92,7 @@ def calculate_phase_2d(x):
 
 
 @mark_dependency
-def mpd(
-    *, obs, pred, return_ignored=False, return_std=False, phase_func_1d=calculate_phase
-):
+def mpd(*, obs, pred, return_ignored=False, return_std=False):
     """Mean phase difference.
 
     Args:
@@ -108,7 +102,7 @@ def mpd(
 
     """
     if len(obs.shape) == 2 and len(pred.shape) == 2:
-        phase_func = phase_func_1d
+        phase_func = calculate_phase
     elif len(obs.shape) == 3 and len(pred.shape) == 3:
         phase_func = calculate_phase_2d
     else:
@@ -225,248 +219,10 @@ def calculate_resampled_errors(*, reference_data, valid_reference_data, total_se
     mpd_errors = np.zeros(N)
     resampled_map = np.ma.MaskedArray(np.zeros_like(reference_data), mask=True)
     for i in tqdm(range(N), desc="Calculating resampling errors"):
-        resampled = np.random.choice(
+        resampled = np.random.default_rng(0).choice(
             valid_reference_data, size=valid_reference_data.size
         )
         nme_errors[i] = nme(obs=valid_reference_data, pred=resampled)
         resampled_map[total_sel] = resampled
         mpd_errors[i] = mpd(obs=reference_data, pred=resampled_map)
     return nme_errors, mpd_errors
-
-
-def error_hist(
-    *, errors, title, error_dict, save_path, title_label=None, fig=None, ax=None
-):
-    if fig is None and ax is None:
-        fig = plt.figure()
-    elif fig is None:
-        fig = ax.get_figure()
-    if ax is None:
-        ax = plt.axes()
-
-    ax.set_title(title)
-
-    if title_label is not None:
-        ax.text(-0.01, 1.05, title_label, transform=ax.transAxes)
-
-    if errors is not None:
-        ax.hist(errors, bins="auto", density=True)
-
-    xmins = [np.min(errors)] if errors is not None else []
-    xmaxs = [np.max(errors)] if errors is not None else []
-
-    ax2 = ax.twinx()
-
-    # Indicate other errors.
-    prev_ylim = ax.get_ylim()
-    for (i, (key, (err, std))) in enumerate(error_dict.items()):
-        ax.vlines(err, *prev_ylim, color=f"C{i+1}", label=key)
-
-        xs = np.linspace(err - std, err + std, 100)
-        ax2.plot(
-            xs,
-            (1 / np.sqrt(2 * np.pi * std**2))
-            * np.exp(-((xs - err) ** 2) / (2 * std**2)),
-            c=f"C{i+1}",
-            linestyle="--",
-            alpha=0.8,
-        )
-
-        xmins.append(err - std)
-        xmaxs.append(err + std)
-
-    ax.set_ylim(*prev_ylim)
-    ax.set_xlim(np.min(xmins), np.max(xmaxs))
-
-    ax.legend(loc="best")
-    if save_path is not None:
-        fig.savefig(save_path)
-        plt.close(fig)
-
-
-def null_model_analysis(
-    reference_data,
-    comp_data=None,
-    rng=None,
-    save_dir=None,
-    N=10000,
-    regions="PNV",
-):
-    """Data should have the 3D shape (12, M, N), i.e. map data over 12 months.
-
-    For N96e, M=144, N=192
-
-    Args:
-        reference_data (12, lat, lon) numpy array: Reference data.
-        comp_data (dict of (12, lat, lon) numpy array): Data to compare to
-            `reference_data`.
-        rng (numpy.random.default_rng):
-        save_dir (save directory):
-        N (int):  Number of resampling operations.
-        regions ({None, "GFED", "PNV"}): If not None, perform a regional error
-            breakdown.
-
-    """
-    total_mask = np.zeros_like(reference_data, dtype=np.bool_)
-
-    if comp_data is None:
-        comp_data = {}
-    else:
-        for data in comp_data.values():
-            assert len(data.shape) == 3
-            total_mask |= np.ma.getmaskarray(data)
-
-    assert reference_data.ndim == 3
-    total_mask |= np.ma.getmaskarray(reference_data)
-    total_sel = ~total_mask
-
-    if rng is None:
-        rng = np.random.default_rng()
-
-    valid_reference_data = np.ma.getdata(reference_data)[total_sel]
-    valid_comp_data = {
-        key: np.ma.getdata(data)[total_sel] for key, data in comp_data.items()
-    }
-
-    nme_errors, mpd_errors = calculate_resampled_errors(
-        reference_data=reference_data,
-        valid_reference_data=valid_reference_data,
-        total_sel=total_sel,
-        N=N,
-    )
-
-    nme_error_dict = {}
-    mpd_error_dict = {}
-
-    # Error given just the mean state.
-    nme_error_dict["mean_state"] = nme(
-        obs=valid_reference_data,
-        pred=np.zeros_like(valid_reference_data) + np.mean(valid_reference_data),
-        return_std=True,
-    )
-    mpd_error_dict["mean_state"] = mpd(
-        obs=reference_data,
-        pred=np.zeros_like(reference_data) + np.mean(valid_reference_data),
-        return_std=True,
-    )
-
-    # Errors for the other data.
-    for key, data in valid_comp_data.items():
-        nme_error_dict[key] = nme(obs=valid_reference_data, pred=data, return_std=True)
-    for key, data in comp_data.items():
-        mpd_error_dict[key] = mpd(obs=reference_data, pred=data, return_std=True)
-
-    # NME Errors.
-    error_hist(
-        errors=nme_errors,
-        title="NME errors (with std of mean)",
-        error_dict=nme_error_dict,
-        save_path=save_dir / "nme_errors.png",
-    )
-
-    # MPD Errors.
-    error_hist(
-        errors=mpd_errors,
-        title="MPD errors (with std of mean)",
-        error_dict=mpd_error_dict,
-        save_path=save_dir / "mpd_errors.png",
-    )
-
-    if regions is not None:
-        # Regional plotting.
-        if regions == "GFED":
-            regions_cube, N_plots = get_gfed_regions()
-        elif regions == "PNV":
-            regions_cube, N_plots = get_pnv_mega_plot_data()
-        else:
-            raise ValueError(f"Unknown regions: '{regions}'.")
-
-        region_nrows_ncols = dict(nrows=math.ceil(N_plots / 2), ncols=2)
-        regions_cube.data.mask |= np.any(total_mask, axis=0)
-
-        nme_fig, nme_axes = plt.subplots(
-            sharex=True, sharey=True, figsize=(9, 9), **region_nrows_ncols
-        )
-        mpd_fig, mpd_axes = plt.subplots(
-            sharex=True, sharey=True, figsize=(9, 9), **region_nrows_ncols
-        )
-
-        for (plot_i, (region_code, region_name)) in enumerate(
-            {
-                code: name
-                for code, name in regions_cube.attributes["short_regions"].items()
-                # Ignore the Ocean region.
-                if code != 0
-            }.items()
-        ):
-            region_sel = (
-                np.ones((12, 1, 1), dtype=np.bool_)
-                & (np.ma.getdata(regions_cube.data) == region_code)[np.newaxis]
-            )
-            # NOTE hist() does not seem to handle masked arrays, so ensure that only valid
-            # entries are passed to it.
-
-            # Regional errors.
-            reg_nme_error_dict = {}
-            reg_mpd_error_dict = {}
-
-            reg_reference_data = np.ma.MaskedArray(
-                np.ma.getdata(reference_data),
-                mask=~(~np.ma.getmaskarray(reference_data) & region_sel),
-            )
-            reg_valid_reference_data = np.ma.getdata(reference_data)[region_sel]
-
-            reg_valid_comp_data = {
-                key: np.ma.getdata(data)[region_sel] for key, data in comp_data.items()
-            }
-            reg_comp_data = {
-                key: np.ma.MaskedArray(
-                    np.ma.getdata(data), mask=~(~np.ma.getmaskarray(data) & region_sel)
-                )
-                for key, data in comp_data.items()
-            }
-
-            for key, data in reg_valid_comp_data.items():
-                reg_nme_error_dict[key] = nme(
-                    obs=reg_valid_reference_data, pred=data, return_std=True
-                )
-            for key, data in reg_comp_data.items():
-                reg_mpd_error_dict[key] = mpd(
-                    obs=reg_reference_data, pred=data, return_std=True
-                )
-
-            title_label = f"({ascii_lowercase[plot_i]})"
-
-            error_hist(
-                errors=None,
-                # Show the region name and number of selected locations.
-                title=f"{region_name} (n={np.sum(region_sel) / 12})",
-                title_label=title_label,
-                error_dict=reg_nme_error_dict,
-                save_path=None,
-                fig=nme_fig,
-                ax=nme_axes.ravel()[plot_i],
-            )
-
-            error_hist(
-                errors=None,
-                # Show the region name and number of selected locations.
-                title=f"{region_name} (n={np.sum(region_sel) / 12})",
-                title_label=title_label,
-                error_dict=reg_mpd_error_dict,
-                save_path=None,
-                fig=mpd_fig,
-                ax=mpd_axes.ravel()[plot_i],
-            )
-
-        nme_fig.suptitle("Regional NME Errors")
-        mpd_fig.suptitle("Regional MPD Errors")
-
-        for fig in [nme_fig, mpd_fig]:
-            fig.tight_layout(rect=[0, 0.0, 1, 0.98])
-
-        nme_fig.savefig(save_dir / "regional_nme_errors.png")
-        mpd_fig.savefig(save_dir / "regional_mpd_errors.png")
-
-        for fig in [nme_fig, mpd_fig]:
-            plt.close(fig)

src/python_inferno/plotting.py
@@ -11,7 +11,7 @@ from loguru import logger
 from matplotlib.transforms import offset_copy
 from wildfires.analysis import cube_plotting
 
-from .ba_model import BAModel
+from .ba_model import ARCSINH_FACTOR, BAModel
 from .configuration import pft_group_names
 from .data import get_2d_cubes, get_gfed_regions, get_pnv_mega_plot_data
 from .metrics import calculate_phase, calculate_phase_2d
@@ -20,7 +20,7 @@ from .utils import wrap_phase_diffs
 
 def lin_cube_plotting(*, data, title, label="BA"):
     cube_plotting(
-        data,
+        data * 1e6,
         title=title,
         nbins=9,
         vmin_vmax_percentiles=(5, 95),
@@ -31,7 +31,7 @@ def lin_cube_plotting(*, data, title, label="BA"):
 
 def log_cube_plotting(*, data, title, raw_data, label="log(BA)"):
     cube_plotting(
-        data,
+        data * 1e6,
         title=title,
         boundaries=np.geomspace(*np.quantile(raw_data[raw_data > 0], [0.05, 0.95]), 8),
         fig=plt.figure(figsize=(12, 5)),
@@ -217,8 +217,6 @@ def plotting(
     raw_data,
     model_ba_2d_data,
     hist_bins,
-    arcsinh_adj_factor,
-    arcsinh_factor,
     scores=None,
     save_dir=None,
     ref_2d_data=None,
@@ -342,7 +340,7 @@ def plotting(
 
     # Global BA map with linear bins and arcsinh transform.
     lin_cube_plotting(
-        data=arcsinh_adj_factor * np.arcsinh(arcsinh_factor * model_ba_2d_data),
+        data=np.arcsinh(ARCSINH_FACTOR * model_ba_2d_data),
         title=title,
         label="arcsinh(a*BA)",
     )