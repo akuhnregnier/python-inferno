663cc50e88aaf001f03b90a80181f970bc76fa2c
Alexander Kuhn-Regnier <ahf.kuhnregnier@gmail.com>
2022-05-30 19:15:10 +0100

    Moved hyperopt optimisation function.

--- src/python_inferno/hyperopt.py
+++ src/python_inferno/hyperopt.py
@@ -1,10 +1,21 @@
 # -*- coding: utf-8 -*-
 import math
+import os
+from pathlib import Path
+from pprint import pformat
+from time import time
 
+import hyperopt
 import numpy as np
 from hyperopt import hp
+from hyperopt.pyll import rec_eval
+from loguru import logger
+from scipy.optimize import basinhopping
 
 from python_inferno.space import OptSpace
+
+from .ba_model import gen_to_optimise
+from .basinhopping import BoundedSteps, Recorder
 
 
 def mod_quniform(name, low, high, q):
@@ -106,3 +117,104 @@ class HyperoptSpace(OptSpace):
             else:
                 raise ValueError("Unsupported `arg_type`.")
         return product
+
+
+def fail_func(*args, **kwargs):
+    return 10000.0
+
+
+def success_func(loss, *args, **kwargs):
+    return loss
+
+
+def main_opt(
+    expr,
+    memo,
+    ctrl,
+    space,
+    dryness_method,
+    fuel_build_up_method,
+    include_temperature,
+):
+    discrete_params = rec_eval(expr, memo=memo)
+    logger.info(f"Discrete parameters:\n{pformat(discrete_params)}")
+
+    start = time()
+
+    # NOTE: These values may differ from `discrete_params` above (but they have the same
+    # keys) if modifications like `mod_quniform` are used.
+    curr_vals = ctrl.current_trial["misc"]["vals"]
+
+    # Fetch previous trials.
+    ctrl.trials.refresh()
+    for trial in ctrl.trials.trials:
+        if (trial["misc"]["vals"] == curr_vals) and (
+            trial["result"]["status"] != "new"
+        ):
+            logger.info(f"Match found (search time: {time() - start:0.2f}).")
+            return trial["result"]
+
+    logger.info(f"No match (search time: {time() - start:0.2f}).")
+
+    to_optimise = gen_to_optimise(
+        fail_func=fail_func,
+        success_func=success_func,
+        # Init (data) params.
+        dryness_method=dryness_method,
+        fuel_build_up_method=fuel_build_up_method,
+        include_temperature=include_temperature,
+        **discrete_params,
+    )
+
+    def to_optimise_with_discrete(x):
+        return to_optimise(
+            **space.inv_map_float_to_0_1(dict(zip(space.continuous_param_names, x)))
+        )
+
+    recorder = Recorder(record_dir=Path(os.environ["EPHEMERAL"]) / "opt_record")
+
+    def basinhopping_callback(x, f, accept):
+        # NOTE: Parameters recorded here are authoritative, since hyperopt will not
+        # properly report values modified as in e.g. `mod_quniform`.
+        values = space.inv_map_float_to_0_1(
+            {**dict(zip(space.continuous_param_names, x)), **discrete_params}
+        )
+        values["dryness_method"] = dryness_method
+        values["fuel_build_up_method"] = fuel_build_up_method
+        values["include_temperature"] = include_temperature
+
+        logger.info(f"Minimum found | loss: {f:0.6f}")
+
+        for name, val in values.items():
+            logger.info(f" - {name}: {val}")
+
+        if recorder is not None:
+            recorder.record(values, f)
+
+            # Update record in file.
+            recorder.dump()
+
+    res = basinhopping(
+        to_optimise_with_discrete,
+        x0=space.continuous_x0_mid,
+        disp=True,
+        minimizer_kwargs=dict(
+            method="L-BFGS-B",
+            jac=None,
+            bounds=[(0, 1)] * len(space.continuous_param_names),
+            options=dict(maxiter=60, ftol=1e-5, eps=1e-3),
+        ),
+        seed=0,
+        T=0.05,
+        niter=100,
+        niter_success=15,
+        callback=basinhopping_callback,
+        take_step=BoundedSteps(stepsize=0.3, rng=np.random.default_rng(0)),
+    )
+
+    loss = res.fun
+
+    if loss > 100.0:
+        return {"loss": 10000.0, "status": hyperopt.STATUS_FAIL}
+    else:
+        return {"loss": loss, "status": hyperopt.STATUS_OK}
